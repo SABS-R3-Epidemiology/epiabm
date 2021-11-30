36e75db2d713caa6328c2073d160a7d7302b2464
Kit Gallagher <gallagherkit@outlook.com>
2021-11-30 12:14:40 +0000

    Update documentation of __init__ files

--- pyEpiabm/pyEpiabm/__init__.py
+++ pyEpiabm/pyEpiabm/__init__.py
@@ -1,3 +1,14 @@
+
+#
+# Root of the pyEpiabm module.
+# Provides access to all shared functionality (classes, simulation, etc.).
+#
+"""pyepiabm is the python backend for epiabm - an Epidemiological
+Agent Based Modelling software package.
+It contains functionality for setting up a population, and tracking the
+evolution of a virus across it, with various visualisation methods.
+"""
+
 from .person import Person  # noqa
 from .population import Population  # noqa
 from .cell import Cell  # noqa
