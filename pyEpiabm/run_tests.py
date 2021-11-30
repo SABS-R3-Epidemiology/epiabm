7766a3dbf3b7781eab2345d432a01a900a323a50
Kit Gallagher <gallagherkit@outlook.com>
2021-11-30 21:47:51 +0000

    Pass style tests

--- pyEpiabm/run_tests.py
+++ pyEpiabm/run_tests.py
@@ -17,7 +17,8 @@ def run_unit_tests():
     """
     Runs unit tests (without subprocesses).
     """
-    tests = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pyEpiabm', 'tests')
+    tests = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                         'pyEpiabm', 'tests')
     suite = unittest.defaultTestLoader.discover(tests, pattern='test*.py')
     res = unittest.TextTestRunner(verbosity=2).run(suite)
     sys.exit(0 if res.wasSuccessful() else 1)
