d202072988fd4709b137ead460a094cafac39ce1
Nicholas Fan <nicholas.lj.fan@gmail.com>
2021-12-01 09:58:20 +0000

    Added Infection Status Enum and Tests

--- pyEpiabm/pyEpiabm/__init__.py
+++ pyEpiabm/pyEpiabm/__init__.py
@@ -13,3 +13,4 @@ from .person import Person  # noqa
 from .population import Population  # noqa
 from .cell import Cell  # noqa
 from .microcell import Microcell  # noqa
+from .infection_status import InfectionStatus # noqa
---
+++ pyEpiabm/pyEpiabm/tests/test_infection_status.py
@@ -0,0 +1,27 @@
+import unittest
+import pyEpiabm as pe
+
+
+class TestCell(unittest.TestCase):
+    """
+    Test the 'InfectionStatus' enum.
+    """
+    
+    def test_construct(self):
+        statuses = [
+            pe.InfectionStatus.Susceptible,
+            pe.InfectionStatus.Exposed,
+            pe.InfectionStatus.InfectASympt,
+            pe.InfectionStatus.InfectMild,
+            pe.InfectionStatus.InfectGP,
+            pe.InfectionStatus.InfectHosp,
+            pe.InfectionStatus.InfectICU,
+            pe.InfectionStatus.InfectICURecov,
+            pe.InfectionStatus.Recovered,
+            pe.InfectionStatus.Dead]
+
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
