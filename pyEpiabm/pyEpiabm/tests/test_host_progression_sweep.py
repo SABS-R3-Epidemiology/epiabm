065b89eb682ceac3a851699b2117786f417f0edd
Elizabeth Hayman <elizabeth.hayman@keble.ox.ac.uk>
2021-12-02 11:00:20 +0000

    host sweep test

--- pyEpiabm/pyEpiabm/tests/test_host_progression_sweep.py
+++ pyEpiabm/pyEpiabm/tests/test_host_progression_sweep.py
@@ -5,25 +5,29 @@ import pyEpiabm as pe
 
 class TestHostProgressionSweep(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        '''Sets up a population we can use throughout the test.
+        2 people in one microcell
+        '''
+        cls.test_population = pe.Population()
+        cls.test_population.add_cells(1)
+        cls.test_cell = cls.test_population.cells[0]
+        cls.test_population.cells[0].add_microcells(1)
+        cls.test_population.cells[0].microcells[0].add_people(2)
+        cls.person1 = cls.test_population.cells[0].microcells[0].persons[0]
+        cls.person2 = cls.test_population.cells[0].microcells[0].persons[1]
+
     def test_construct(self):
         pe.HostProgressionSweep()
 
     def test_update_status(self):
-        # Create a test population with 2 people in one microcell
-        test_population = pe.Population()
-        test_population.add_cells(1)
-        test_cell = test_population.cells[0]
-        test_population.cells[0].add_microcells(1)
-        test_population.cells[0].microcells[0].add_people(2)
-        person1 = test_population.cells[0].microcells[0].persons[0]
-        person2 = test_population.cells[0].microcells[0].persons[1]
-
         # set person 2 to exposed and becoming infectious in one time step
-        person2.infection_status = pe.InfectionStatus.InfectMild
+        self.person2.infection_status = pe.InfectionStatus.InfectMild
         test_sweep = pe.HostProgressionSweep()
-        test_sweep.bind_population(test_population)
-        test_sweep._update_next_infection_status(person2)
-        self.assertEqual(person2.next_infection_status, pe.InfectionStatus.Recovered)
+        test_sweep.bind_population(self.test_population)
+        test_sweep._update_next_infection_status(self.person2)
+        self.assertEqual(self.person2.next_infection_status, pe.InfectionStatus.Recovered)
 
     def test_update_time(self):
         test_sweep = pe.HostProgressionSweep()
@@ -32,36 +36,25 @@ class TestHostProgressionSweep(unittest.TestCase):
         self.assertTrue(1 <= time <= 10)
 
     def test_call(self):
-        # Create a test population with 2 people in one microcell
-        test_population = pe.Population()
-        test_population.add_cells(1)
-        test_cell = test_population.cells[0]
-        test_population.cells[0].add_microcells(1)
-        test_microcell = test_cell.microcells[0]
-        test_population.cells[0].microcells[0].add_people(2)
-        person1 = test_population.cells[0].microcells[0].persons[0]
-        person2 = test_population.cells[0].microcells[0].persons[1]
-
         # set person 2 to exposed and becoming infectious in one time step
-        person2.infection_status = pe.InfectionStatus.Exposed
-        person2.time_of_status_change = 1
-        person2.next_infection_status = pe.InfectionStatus.InfectMild
+        self.person2.infection_status = pe.InfectionStatus.Exposed
+        self.person2.time_of_status_change = 1
+        self.person2.next_infection_status = pe.InfectionStatus.InfectMild
         test_sweep = pe.HostProgressionSweep()
-        test_sweep.bind_population(test_population)
-        print(person2.time_of_status_change)
+        test_sweep.bind_population(self.test_population)
 
         # tests population bound successfully
         self.assertEqual(test_sweep._population.cells[0].persons[1].infection_status, pe.InfectionStatus.Exposed)
 
         test_sweep(1)
-        self.assertEqual(person2.infection_status,
+        self.assertEqual(self.person2.infection_status,
                          pe.InfectionStatus.InfectMild)
-        self.assertEqual(person2.next_infection_status,
+        self.assertEqual(self.person2.next_infection_status,
                          pe.InfectionStatus.Recovered)
-        self.assertEqual(person1.infection_status,
+        self.assertEqual(self.person1.infection_status,
                          pe.InfectionStatus.Susceptible)
-        self.assertIsInstance(person2.time_of_status_change, int)
-        self.assertTrue(2 <= person2.time_of_status_change <= 11)
+        self.assertIsInstance(self.person2.time_of_status_change, int)
+        self.assertTrue(2 <= self.person2.time_of_status_change <= 11)
 
 
 if __name__ == '__main__':
