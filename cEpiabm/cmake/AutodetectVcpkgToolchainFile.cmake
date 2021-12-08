3ab0c23517e6b68c9ff5b980da377b5f76880c9d
Nicholas Fan <nicholas.lj.fan@gmail.com>
2021-12-08 14:07:33 +0000

    Update

--- cEpiabm/cmake/AutodetectVcpkgToolchainFile.cmake
+++
@@ -1,8 +0,0 @@
-# If a VCPKG toolchain file is not defined, but the expected file exists, use it
-if (NOT DEFINED CMAKE_TOOLCHAIN_FILE)
-    if (EXISTS ${CMAKE_SOURCE_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake)
-        set(vcpkg_toolchain_file ${CMAKE_SOURCE_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake)
-        message(STATUS "Detected vcpkg toolchain file at ${vcpkg_toolchain_file}")
-        set(CMAKE_TOOLCHAIN_FILE ${vcpkg_toolchain_file})
-    endif ()
-endif ()
