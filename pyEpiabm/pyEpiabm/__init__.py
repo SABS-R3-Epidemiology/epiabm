0c0ec02a722b5d3446d3af0fb2c67174fa1380ba
Elizabeth Hayman <elizabeth.hayman@keble.ox.ac.uk>
2021-12-08 15:39:41 +0000

    started tests

--- pyEpiabm/pyEpiabm/__init__.py
+++ pyEpiabm/pyEpiabm/__init__.py
@@ -11,6 +11,7 @@ evolution of a virus across it, with various visualisation methods.
 
 from .person import Person  # noqa
 from .population import Population  # noqa
+from .toy_population_config import ToyPopulation  # noqa
 from .cell import Cell  # noqa
 from .microcell import Microcell  # noqa
 from .infection_status import InfectionStatus # noqa
---
+++ pyEpiabm/pyEpiabm/tests/test_toy_population_config.py
@@ -0,0 +1,7 @@
+import unittest
+import pyEpiabm as pe
+
+
+class TestTopPopConfig(unittest.TestCase):
+    """Test the Toy Population class.
+    """
