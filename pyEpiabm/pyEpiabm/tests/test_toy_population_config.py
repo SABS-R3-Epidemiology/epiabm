375f13a13664ac23d97152b65b258a0b7281a794
Elizabeth Hayman <elizabeth.hayman@keble.ox.ac.uk>
2021-12-13 12:28:31 +0000

    factorised class

--- pyEpiabm/pyEpiabm/tests/test_toy_population_config.py
+++ pyEpiabm/pyEpiabm/tests/test_toy_population_config.py
@@ -1,31 +1,34 @@
 import unittest
 import pyEpiabm as pe
+from parameterized import parameterized
+import random
 from pyEpiabm.toy_population_config import ToyPopulation
 
+numReps = 1
 
-class TestTopPopConfig(unittest.TestCase):
+
+class TestPopConfig(unittest.TestCase):
     """Test the Toy Population class.
     """
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls.pop_size = 1000
-        cls.cell_number = 5
-        cls.microcell_per_cell = 5
-        cls.household_number = 10
-
-    def test_init(self):
+    @parameterized.expand([(random.randint(1000, 10000),
+                            random.randint(1, 10),
+                            random.randint(1, 10),
+                            random.randint(1, 10))
+                          for _ in range(numReps)])
+    def test_make_pop(self, pop_size, cell_number, microcell_per_cell,
+                      household_number):
         """Tests for when the population is implemented by default with
         no households.
         """
         # Population is initialised with no households
-        self.toy_pop = pe.ToyPopulation(self.pop_size, self.cell_number,
-                                        self.microcell_per_cell,
-                                        self.household_number)
+        test_pop = pe.ToyPopulation().make_pop(pop_size, cell_number,
+                                               microcell_per_cell,
+                                               household_number)
 
         total_people = 0
         count_non_empty_cells = 0
         count_non_trivial_households = 0
-        for cell in self.toy_pop.population.cells:
+        for cell in test_pop.cells:
             for microcell in cell.microcells:
                 total_people = total_people + len(microcell.persons)
                 for person in microcell.persons:
@@ -33,34 +36,43 @@ class TestTopPopConfig(unittest.TestCase):
                         count_non_trivial_households += 1
             if len(cell.persons) > 0:
                 count_non_empty_cells += 1
-        # test there are at least one non-empty cell
+        # test there are at least one non-empty cell.
         self.assertTrue(count_non_empty_cells > 1)
 
-        # test that everyone in the population has been assigned a microcell
-        self.assertEqual(total_people, self.pop_size)
+        # test that everyone in the population has been assigned a microcell.
+        self.assertEqual(total_people, pop_size)
 
-        # test that each household is trivial (contains one person)
+        # test that each household is trivial (contains one person).
         self.assertEqual(count_non_trivial_households, 0)
 
-    def test_if_households(self):
+        # test a population class object is returned.
+        self.assertIsInstance(test_pop, pe.Population)
+
+    @parameterized.expand([(random.randint(1000, 10000) * numReps,
+                            random.randint(1, 10) * numReps,
+                            random.randint(1, 10) * numReps,
+                            random.randint(1, 10) * numReps)
+                          for _ in range(numReps)])
+    def test_if_households(self, pop_size, cell_number, microcell_per_cell,
+                           household_number):
         """Tests when households are implemented.
         """
         if_households = 1.0
         # Tests that if_households only takes boolean input.
-        self.assertRaises(TypeError, ToyPopulation, self.pop_size,
-                          self.cell_number, self.microcell_per_cell,
-                          self.household_number,
+        self.assertRaises(TypeError, ToyPopulation().make_pop, pop_size,
+                          cell_number, microcell_per_cell,
+                          household_number,
                           if_households)
         # Initialises population with households.
         if_households = True
-        self.toy_pop = pe.ToyPopulation(self.pop_size, self.cell_number,
-                                        self.microcell_per_cell,
-                                        self.household_number,
-                                        if_households)
+        toy_pop = pe.ToyPopulation().make_pop(pop_size, cell_number,
+                                              microcell_per_cell,
+                                              household_number,
+                                              if_households)
         total_people = 0
         households = []
         num_empty_households = 0
-        for cell in self.toy_pop.population.cells:
+        for cell in toy_pop.cells:
             for microcell in cell.microcells:
                 for person in microcell.persons:
                     if person.household not in households:
@@ -69,9 +81,8 @@ class TestTopPopConfig(unittest.TestCase):
                         num_empty_households += 1
                     total_people += len(person.household.persons)
         # Some households may be empty so won't be included.
-        # Tests that max 10% of the total implemented are empty.
-        total_households = self.cell_number * self.microcell_per_cell \
-            * self.household_number
+        total_households = cell_number * microcell_per_cell \
+            * household_number
         self.assertTrue(len(households) <= total_households)
         self.assertTrue(num_empty_households < total_households)
 
--- pyEpiabm/pyEpiabm/toy_population_config.py
+++ pyEpiabm/pyEpiabm/toy_population_config.py
@@ -4,11 +4,12 @@ from .household import Household
 
 
 class ToyPopulation:
-    """ Class that creates a toy population for use in the simple python model.
+    """ Class that creates a toy population for use and returns in the simple
+    python model.
     """
-    def __init__(self, pop_size: int, cell_number: int,
+    def make_pop(self, population_size: int, cell_number: int,
                  microcell_per_cell: int, household_number: int,
-                 if_households=False):
+                 if_households: bool = False):
         """Method that initializes a population class with a given population size,
         number of cells and number of microcells. A multinomial distribution is
         used to distribute the number of people into the different microcells.
@@ -25,39 +26,50 @@ class ToyPopulation:
         : type household_number: int
         : param if_households: decides whether to put people into households.
         : type if_households: bool
+
+        : return new_pop: Population
+        : rtype new_pop: `Population` class object
         """
+        # Initialise a population class
+        new_pop = Population()
+
+        # Checks parameter type and stores as class objects
         if not (type(if_households) == bool):
             raise TypeError('Include household input needs to be boolean')
-        self.pop_size = pop_size
-        self.population = Population()
-        self.population.add_cells(cell_number)
+        self.pop_size = population_size
         self.total_number_microcells = cell_number*microcell_per_cell
         self.if_households = if_households
         self.household_number = household_number
 
+        new_pop.add_cells(cell_number)
+        # Sets up a probability array for the multinomial
         p = [1/self.total_number_microcells]*self.total_number_microcells
-        cell_split = np.random.multinomial(pop_size, p, size=1)[0]
+        # Distributes multinomially people into microcells
+        cell_split = np.random.multinomial(population_size, p, size=1)[0]
         i = 0
-        for cell in self.population.cells:
+        for cell in new_pop.cells:
             cell.add_microcells(microcell_per_cell)
             for microcell in cell.microcells:
                 people_in_microcell = cell_split[i]
                 microcell.add_people(people_in_microcell)
                 i += 1
 
-        if self.if_households:
-            self.add_households()
+        # Splits people into households, either with a given number of
+        # housholds per cell, or trivially (each person gets their own
+        # household).
+        if if_households:
+            self.add_households(new_pop)
         else:
-            for cells in self.population.cells:
-                for person in cells.persons:
-                    new_household = Household()
-                    new_household.add_person(person)
+            self.no_households(new_pop)
 
-    def add_households(self):
+        return new_pop
+
+    def add_households(self, population: Population):
         """Method that groups people in microcell into households together.
         """
+        # Initialises another multinomial distribution
         q = [1/self.household_number]*self.household_number
-        for cell in self.population.cells:
+        for cell in population.cells:
             for microcell in cell.microcells:
                 people_number = len(microcell.persons)
                 household_split = np.random.multinomial(people_number, q,
@@ -66,7 +78,17 @@ class ToyPopulation:
                 for j in range(self.household_number):
                     people_in_household = household_split[j]
                     new_household = Household()
-                    for k in range(people_in_household):
+                    for _ in range(people_in_household):
                         person = microcell.persons[person_index]
                         new_household.add_person(person)
                         person_index += 1
+
+    def no_households(self, population: Population):
+        """Method assigns each person to their own individual household.
+        This means that household sweep will run trivially instead of
+        throwing an error.
+        """
+        for cells in population.cells:
+            for person in cells.persons:
+                new_household = Household()
+                new_household.add_person(person)
