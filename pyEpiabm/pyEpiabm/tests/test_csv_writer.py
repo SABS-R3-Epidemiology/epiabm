0842349b3d278a493991ee60a098be63ef876ca0
Elizabeth Hayman <elizabeth.hayman@keble.ox.ac.uk>
2021-12-16 12:07:32 +0000

    unfinished tests

---
+++ pyEpiabm/pyEpiabm/tests/test_csv_writer.py
@@ -0,0 +1,26 @@
+import unittest
+import pyEpiabm as pe
+from unittest.mock import patch, mock_open
+import os
+
+
+class TestCsvWriter(unittest.TestCase):  # should it be Test_CsvWriter?
+    """Test the '_CsvWriter' class.
+    """
+
+    def test_init(self):
+        mo = mock_open()
+        with patch('pyEpiabm._csv_writer.open', mo):
+            mock_content = ['1', '2', '3']
+            m = pe._CsvWriter('mock_filename', mock_content)
+            del(m)
+        #m.mock_calls
+        #m.assert_called_once_with('mock_filename', mock_content)
+        #m().write.assert_called_once_with(mock_content)
+        print(mo().write)
+        #content = mock_file.read()
+        #print(content.__dict__)
+        #self.assertEqual(content, '1, 2, 3')
+
+if __name__ == '__main__':
+    unittest.main()
--- pyEpiabm/pyEpiabm/tests/test_simulation.py
+++ pyEpiabm/pyEpiabm/tests/test_simulation.py
@@ -1,6 +1,6 @@
 import unittest
 import pyEpiabm as pe
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 
 class TestSimulation(unittest.TestCase):
@@ -23,11 +23,13 @@ class TestSimulation(unittest.TestCase):
 
     def test_configure(self):
         initial_sweeps = [0.5]
+        file_params = MagicMock()
+        file_params.return_value = 'test_file'
         test_sim = pe.Simulation()
         # Test assertion raised when sweep lists don't contain sweeps.
         self.assertRaises(AssertionError, test_sim.configure,
                           self.test_population, initial_sweeps,
-                          self.sweeps, self.sim_params, self.file_params)
+                          self.sweeps, self.sim_params, file_params)
         # Test configure binds parameters as expected.
         test_sim.configure(self.test_population, self.initial_sweeps,
                            self.sweeps, self.sim_params, self.file_params)
@@ -40,21 +42,25 @@ class TestSimulation(unittest.TestCase):
     @patch('pyEpiabm.InitialInfectedSweep.__call__')
     @patch('pyEpiabm.Simulation.write_to_file')
     def test_run_sweeps(self, patch_write, patch_initial, patch_sweep):
+        file_params = MagicMock()
+        file_params.return_value = 'test_file'
         time_sweep = self.sim_params["simulation_start_time"] + 1
         time_write = self.sim_params["simulation_end_time"] - 1
         test_sim = pe.Simulation()
         test_sim.configure(self.test_population, self.initial_sweeps,
-                           self.sweeps, self.sim_params, self.file_params)
+                           self.sweeps, self.sim_params, file_params)
         test_sim.run_sweeps()
         patch_initial.assert_called_with(self.sim_params)
         patch_sweep.assert_called_with(time_sweep)
         patch_write.assert_called_with(time_write)
 
     def test_write_to_file(self):
+        file_params = MagicMock()
+        file_params.return_value = 'test_file'
         time = 1
         test_sim = pe.Simulation()
         test_sim.configure(self.test_population, self.initial_sweeps,
-                           self.sweeps, self.sim_params, self.file_params)
+                           self.sweeps, self.sim_params, file_params)
         data = {s: 0 for s in list(pe.InfectionStatus)}
         data["time"] = time
         with patch.object(test_sim.writer, 'write') as mock:
