fdddfae03837e2090ce92c92f59b4a659cab80ee
Patricia Lamirande <patricia.lamirande@dtc.ox.ac.uk>
2021-12-16 15:01:39 +0000

    Completed tests for CSV writer methods

--- pyEpiabm/pyEpiabm/tests/test_csv_writer.py
+++ pyEpiabm/pyEpiabm/tests/test_csv_writer.py
@@ -1,13 +1,15 @@
 import unittest
 import pyEpiabm as pe
-from unittest.mock import patch, mock_open, call
+from unittest.mock import patch, mock_open, call, MagicMock
 
 
-class TestCsvWriter(unittest.TestCase):  # should it be Test_CsvWriter?
-    """Test the '_CsvWriter' class.
+class TestCsvWriter(unittest.TestCase):
+    """Test the three methods of the '_CsvWriter' class.
     """
 
     def test_init(self):
+        """Test the __init__ method of the _CsvWriter class.
+        """
         mo = mock_open()
         with patch('pyEpiabm._csv_writer.open', mo):
             mock_content = ['1', '2', '3']
@@ -17,17 +19,25 @@ class TestCsvWriter(unittest.TestCase):  # should it be Test_CsvWriter?
         mo().write.assert_called_once_with('1,2,3\r\n')
 
     def test_write(self):
-        # Set up of the file
+        """Test the write method of the _CsvWriter class.
+        """
         mo = mock_open()
         with patch('pyEpiabm._csv_writer.open', mo):
             mock_content = ['1', '2', '3']
             new_content = ['a', 'b', 'c']
             m = pe._CsvWriter('mock_filename', mock_content)
             m.write(new_content)
-        # Test to add a row
         mo().write.assert_has_calls([call('1,2,3\r\n'), call('a,b,c\r\n')])
 
-# Should we test __del__ or is it implicitly used somewhere above?
+    def test_del(self):
+        """Test the __del__ method of the _CsvWriter class.
+        """
+        fake_file = MagicMock()
+        with patch("builtins.open", return_value=fake_file, create=True):
+            mock_content = ['1', '2', '3']
+            m = pe._CsvWriter('mock_filename', mock_content)
+            m.__del__()
+            fake_file.close.assert_called_once()
 
 
 if __name__ == '__main__':
