d7682d2bff96225bd39b5e362efc8cfabb576d57
Kit Gallagher <gallagherkit@outlook.com>
2022-01-24 14:06:02 +0000

    Add test to population_seed

--- pyEpiabm/pyEpiabm/tests/test_routine/test_toy_population_config.py
+++ pyEpiabm/pyEpiabm/tests/test_routine/test_toy_population_config.py
@@ -32,14 +32,59 @@ class TestPopConfig(unittest.TestCase):
                 total_people += len(microcell.persons)
             if len(cell.persons) > 0:
                 count_non_empty_cells += 1
-        # test there are at least one non-empty cell.
+        # Test there are at least one non-empty cell
         self.assertTrue(count_non_empty_cells >= 1)
 
-        # test that everyone in the population has been assigned a microcell.
+        # Test that everyone in the population has been assigned a microcell
         self.assertEqual(total_people, pop_size)
 
-        # test a population class object is returned.
+        # Test a population class object is returned
         self.assertIsInstance(test_pop, pe.Population)
+
+    def summarise_pop(self, pop):
+        # Returns lists of cell and microcell wise populations
+        # Not a testing function, but used in test below
+
+        pop_cells = []  # List of populations in each cell of population
+        pop_microcells = []  # List of populations in each microcell
+        for cell in pop.cells:
+            pop_cells.append(len(cell.persons))
+            for microcell in cell.microcells:
+                pop_microcells.append(len(microcell.persons))
+        return pop_cells, pop_microcells
+
+    @parameterized.expand([(random.randint(1000, 10000),
+                            random.randint(5, 10),
+                            random.randint(2, 10),
+                            random.randint(1, 100))
+                          for _ in range(numReps)])
+    def test_pop_seed(self, pop_size, cell_number, microcell_number, seed):
+        """Tests for when the population is implemented by default with
+        no households. Parameters are assigned at random.
+        """
+        # Define parameters for population generation
+        pop_params = {"population_size": pop_size, "cell_number": cell_number,
+                      "microcell_number": microcell_number,
+                      "population_seed": seed}
+
+        # Create two identical populations with the same seed
+        seed_pop = ToyPopulationFactory().make_pop(pop_params)
+        comp_pop = ToyPopulationFactory().make_pop(pop_params)
+
+        self.assertEqual(str(seed_pop), str(comp_pop))
+
+        seed_cells, seed_microcells = self.summarise_pop(seed_pop)
+        comp_cells, comp_microcells = self.summarise_pop(comp_pop)
+        self.assertEqual(seed_cells, comp_cells)
+        self.assertEqual(seed_microcells, comp_microcells)
+
+        # Also compare to a population with a different seed
+        pop_params["population_seed"] = seed + 1  # Change seed of population
+        diff_pop = ToyPopulationFactory().make_pop(pop_params)
+
+        diff_cells, diff_microcells = self.summarise_pop(diff_pop)
+        self.assertNotEqual(seed_cells, diff_cells)
+        self.assertNotEqual(seed_microcells, diff_microcells)
 
     @parameterized.expand([(random.randint(1000, 10000) * numReps,
                             random.randint(1, 10) * numReps,
@@ -51,7 +96,7 @@ class TestPopConfig(unittest.TestCase):
         """Tests when households are implemented.
         """
 
-        # Initialises population with households.
+        # Initialises population with households
         pop_params = {"population_size": pop_size, "cell_number": cell_number,
                       "microcell_number": microcell_number,
                       "household_number": household_number}
@@ -67,7 +112,7 @@ class TestPopConfig(unittest.TestCase):
                     if len(person.household.persons) == 0:
                         num_empty_households += 1
                     total_people += len(person.household.persons)
-        # Some households may be empty so won't be included.
+        # Some households may be empty so won't be included
         total_households = cell_number * microcell_number \
             * household_number
         self.assertTrue(len(households) <= total_households)
@@ -83,7 +128,7 @@ class TestPopConfig(unittest.TestCase):
         """Tests when places are implemented.
         """
 
-        # Initialises population with places.
+        # Initialises population with places
         pop_params = {"population_size": pop_size, "cell_number": cell_number,
                       "microcell_number": microcell_number,
                       "place_number": place_number}
@@ -94,7 +139,7 @@ class TestPopConfig(unittest.TestCase):
             for microcell in cell.microcells:
                 places += microcell.places
 
-        # Test the correct number of place shave been added to each microcell.
+        # Test the correct number of place shave been added to each microcell
         self.assertEqual(place_number,
                          len(toy_pop.cells[0].microcells[0].places))
 
