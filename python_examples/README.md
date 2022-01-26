7e351569e2478340b4e0e924e98cd07fa09dff0f
Kit Gallagher <gallagherkit@outlook.com>
2022-01-26 15:49:20 +0000

    Basic spatial workflow (#28)

---
+++ python_examples/README.md
@@ -0,0 +1,15 @@
+# Python Examples
+
+This directory contains a number of examples of python workflows to run simulations with pEpiabm.
+
+## Simulation Flow
+
+This script runs a basic simulation with a population of 100 individuals, spread across 20 households in a single cell and microcell.
+
+It subsequently saves the results to a .csv file, and plots a basic SIR plot of the simulation output.
+
+## Simulation Flow - Spatial
+
+This script runs a basic simulation with spatial dependance. It considers a population of 1000 individuals, spread across 40 households in 20 cells, each with a single microcell.
+
+It subsequently saves the results to a .csv file, and plots the infection curve for each region.
---
+++ python_examples/simulation_flow_spatial.py
@@ -0,0 +1,60 @@
+#
+# Example simulation script with spatial data output and visualisation
+#
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import pyEpiabm as pe
+
+# Method to set the seed at the start of the simulation, for reproducibility
+
+pe.routine.Simulation.set_random_seed(seed=42)
+
+# Pop_params are used to configure the population structure being used in this
+# simulation.
+
+pop_params = {"population_size": 1000, "cell_number": 20,
+              "microcell_number": 1, "household_number": 2,
+              "place_number": 2}
+
+pe.Parameters.instance().time_steps_per_day = 1
+
+# Create a population based on the parameters given.
+population = pe.routine.ToyPopulationFactory().make_pop(pop_params)
+cell = population.cells[0]
+
+# sim_ and file_params give details for the running of the simulations and
+# where output should be written to.
+sim_params = {"simulation_start_time": 0, "simulation_end_time": 60,
+              "initial_infected_number": 5}
+
+file_params = {"output_file": "output.csv",
+               "output_dir": "python_examples/spatial_outputs"}
+
+# Create a simulation object, configure it with the parameters given, then
+# run the simulation.
+sim = pe.routine.Simulation()
+sim.configure(
+    population,
+    [pe.sweep.InitialInfectedSweep()],
+    [pe.sweep.UpdatePlaceSweep(), pe.sweep.HouseholdSweep(),
+     pe.sweep.PlaceSweep(), pe.sweep.QueueSweep(),
+     pe.sweep.HostProgressionSweep()],
+    sim_params,
+    file_params)
+sim.run_sweeps()
+
+# Need to close the writer object at the end of each simulation.
+del(sim.writer)
+del(sim)
+
+# Creation of a plot of results
+filename = os.path.join(os.path.dirname(__file__), "spatial_outputs",
+                        "output.csv")
+df = pd.read_csv(filename)
+df.plot(x="time", y=["InfectionStatus.Susceptible",
+                     "InfectionStatus.InfectMild",
+                     "InfectionStatus.Recovered"])
+plt.show()
