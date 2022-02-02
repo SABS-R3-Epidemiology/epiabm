0179229a2c3ec4769460d60237a06eec84e9ef44
Luke Heirene <luke.heirene@dtc.ox.ac.uk>
2022-02-02 15:28:15 +0000

    adjusted unit test so coverage is 100%

--- pyEpiabm/pyEpiabm/tests/test_utility/test_random_methods.py
+++ pyEpiabm/pyEpiabm/tests/test_utility/test_random_methods.py
@@ -26,6 +26,7 @@ class TestInfectionStatus(unittest.TestCase):
         """Tests that the covid_sim_rand method returns the
         correct random numbers"""
 
+        # Tests covid_sim_rand returns correct outputs
         """expected_outputs is a list of the first five outputs of the
         covid_sim_rand method when initialised with the seed s1=163858
         and s2=6573920043. As this seed won't be initialised with any
@@ -42,6 +43,27 @@ class TestInfectionStatus(unittest.TestCase):
             outputs.append(r.covid_sim_rand())
 
         self.assertAlmostEqual(outputs, expected_outputs)
+
+        # Tests checks for negative s1, s2 in algorithm work
+
+        # s1 and s2 values that give negative values that trigger the check
+        r.prev_s1 = 10000000000
+        r.prev_s2 = 10000000000
+
+        # Set parameters and calculate expected s1 and s2 values
+        Xm1 = 2147483563
+        Xm2 = 2147483399
+        Xa1 = 40014
+        Xa2 = 40692
+        k = r.prev_s1 // 53668
+        expected_s1 = (Xa1 * (r.prev_s1 - k * 53668) - k * 12211) + Xm1
+        k1 = r.prev_s2 // 52774
+        expected_s2 = (Xa2 * (r.prev_s2 - k1 * 52774) - k1 * 3791) + Xm2
+
+        # Calculate algorithm successfully adjusts the case of negative s1, s2
+        r.covid_sim_rand()
+        self.assertAlmostEqual(expected_s1, r.prev_s1)
+        self.assertAlmostEqual(expected_s2, r.prev_s2)
 
 
 if __name__ == '__main__':
