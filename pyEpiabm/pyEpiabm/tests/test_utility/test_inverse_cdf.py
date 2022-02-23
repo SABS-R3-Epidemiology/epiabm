d92768b50aacb6e155a0c73f6001cd9dddd32636
Patricia Lamirande <patricia.lamirande@dtc.ox.ac.uk>
2022-02-23 17:03:44 +0000

    Fixed test for inverse cdf

--- pyEpiabm/pyEpiabm/tests/test_utility/test_inverse_cdf.py
+++ pyEpiabm/pyEpiabm/tests/test_utility/test_inverse_cdf.py
@@ -3,24 +3,26 @@ import numpy as np
 from pyEpiabm.utility import InverseCdf
 
 
-def test_construct(self):
-    mean = 5
-    icdf_array = np.ones(20)
-    icdf_object = InverseCdf(mean, icdf_array)
-    self.assertEqual(icdf_object.mean, 5)
-    self.assertEqual(icdf_object.icdf_array, np.array([0.25, 0.50, 0.75]))
+class TestInverseCdf(unittest.TestCase):
+    """Test the 'InverseCdf' class.
+    """
 
+    def test_construct(self):
+        mean = 5
+        icdf_array = np.ones(20)
+        icdf_object = InverseCdf(mean, icdf_array)
+        self.assertEqual(icdf_object.mean, 5)
+        self.assertTrue((icdf_object.icdf_array == np.ones(20)).all())
 
-def test_choose_noexp(self):
-    icdf_object = InverseCdf(3, np.ones(20))
-    value = icdf_object.icdf_choose_noexp()
-    self.assertTrue(0 <= value)
+    def test_choose_noexp(self):
+        icdf_object = InverseCdf(3, np.ones(20))
+        value = icdf_object.icdf_choose_noexp()
+        self.assertTrue(0 <= value)
 
-
-def test_choose_exp(self):
-    icdf_object = InverseCdf(3, np.ones(20))
-    value = icdf_object.icdf_choose_exp()
-    self.assertTrue(0 <= value)
+    def test_choose_exp(self):
+        icdf_object = InverseCdf(3, np.ones(20))
+        value = icdf_object.icdf_choose_exp()
+        self.assertTrue(0 <= value)
 
 
 if __name__ == '__main__':
