065b6f264a8ab6c61b53f817b2f6b64c23f10129
Kit Gallagher <gallagherkit@outlook.com>
2022-03-02 09:58:11 +0000

    Update version number (#73)

--- pyEpiabm/docs/source/conf.py
+++ pyEpiabm/docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, SABS-R3-Epidemiology'
 author = 'SABS-R3-Epidemiology'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+release = '0.0.3'
 
 
 # -- General configuration ---------------------------------------------------
