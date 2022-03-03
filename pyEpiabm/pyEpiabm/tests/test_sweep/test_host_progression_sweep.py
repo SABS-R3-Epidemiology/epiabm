7548bdd463b9325487844e9ccf493c96285d1d0b
Patricia Lamirande <patricia.lamirande@dtc.ox.ac.uk>
2022-03-03 17:05:35 +0000

    Fixed most of flake8 issues

--- pyEpiabm/pyEpiabm/tests/test_sweep/test_host_progression_sweep.py
+++ pyEpiabm/pyEpiabm/tests/test_sweep/test_host_progression_sweep.py
@@ -120,42 +120,46 @@ class TestHostProgressionSweep(unittest.TestCase):
         test_sweep.state_transition_matrix = identity_matrix
         for person in self.people:
             test_sweep._update_next_infection_status(person)
-            self.assertEqual(person.infection_status, person.next_infection_status)
+            self.assertEqual(person.infection_status,
+                             person.next_infection_status)
 
-        # Check that method works for each infection status with all people going
-        # to dead infection status
+        # Check that method works for each infection status with all people
+        # going to dead infection status.
         matrix = np.zeros([len(InfectionStatus), len(InfectionStatus)])
         matrix[:, -1] = 1
         matrix = pd.DataFrame(matrix,
-                                columns=['Susceptible', 'Exposed',
-                                            'InfectASympt', 'InfectMild',
-                                            'InfectGP', 'InfectHosp',
-                                            'InfectICU', 'InfectICURecov',
-                                            'Recovered', 'Dead'],
-                                index=['Susceptible', 'Exposed',
-                                          'InfectASympt', 'InfectMild',
-                                          'InfectGP', 'InfectHosp',
-                                          'InfectICU', 'InfectICURecov',
-                                          'Recovered', 'Dead'])
+                              columns=['Susceptible', 'Exposed',
+                                       'InfectASympt', 'InfectMild',
+                                       'InfectGP', 'InfectHosp',
+                                       'InfectICU', 'InfectICURecov',
+                                       'Recovered', 'Dead'],
+                              index=['Susceptible', 'Exposed',
+                                     'InfectASympt', 'InfectMild',
+                                     'InfectGP', 'InfectHosp',
+                                     'InfectICU', 'InfectICURecov',
+                                     'Recovered', 'Dead'])
         test_sweep.state_transition_matrix = matrix
         for person in self.people:
             test_sweep._update_next_infection_status(person)
-            self.assertEqual(person.next_infection_status, InfectionStatus.Dead)
-        
-        # Check that method works for an upper triangular state transition matrix.
-        random_matrix = np.random.rand(len(InfectionStatus), len(InfectionStatus))
+            self.assertEqual(person.next_infection_status,
+                             InfectionStatus.Dead)
+
+        # Check that method works for an upper triangular state transition
+        # matrix.
+        random_matrix = np.random.rand(len(InfectionStatus),
+                                       len(InfectionStatus))
         random_matrix = np.triu(random_matrix)
         random_matrix = pd.DataFrame(random_matrix,
-                                   columns=['Susceptible', 'Exposed',
+                                     columns=['Susceptible', 'Exposed',
+                                              'InfectASympt', 'InfectMild',
+                                              'InfectGP', 'InfectHosp',
+                                              'InfectICU', 'InfectICURecov',
+                                              'Recovered', 'Dead'],
+                                     index=['Susceptible', 'Exposed',
                                             'InfectASympt', 'InfectMild',
                                             'InfectGP', 'InfectHosp',
                                             'InfectICU', 'InfectICURecov',
-                                            'Recovered', 'Dead'],
-                                   index=['Susceptible', 'Exposed',
-                                          'InfectASympt', 'InfectMild',
-                                          'InfectGP', 'InfectHosp',
-                                          'InfectICU', 'InfectICURecov',
-                                          'Recovered', 'Dead'])
+                                            'Recovered', 'Dead'])
         test_sweep.state_transition_matrix = random_matrix
         for person in self.people:
             test_sweep._update_next_infection_status(person)
@@ -163,94 +167,95 @@ class TestHostProgressionSweep(unittest.TestCase):
             next_enum_value = person.next_infection_status.value
             self.assertTrue(current_enum_value <= next_enum_value)
 
-
     @mock.patch('pyEpiabm.utility.InverseCdf.icdf_choose_exp')
     def test_call_main(self, mock_latent):
-         """Tests the main function of the Host Progression Sweep.
-         Person 1 is set to susceptible and becoming exposed. Person 2 is set to
-         exposed and becoming infectious in one time step. Checks the
-         population updates as expected. Check that Person 3 stays as
-         susceptible.
-         """
-         mock_latent.return_value = 1.0
-         # First check that people progress through the
-         # infection stages correctly.
-         self.person2.update_status(pe.property.InfectionStatus.Exposed)
-         self.person2.time_of_status_change = 1.0
-         self.person2.next_infection_status = \
-             pe.property.InfectionStatus.InfectMild
-         self.person1.update_status(pe.property.InfectionStatus.Susceptible)
-         self.person1.time_of_status_change = 1.0
-         self.person1.next_infection_status = \
-             pe.property.InfectionStatus.Exposed
-         test_sweep = pe.sweep.HostProgressionSweep()
-         self.state_transition_matrix = pe.Parameters.instance().state_transition_matrix
-         test_sweep.bind_population(self.test_population1)
+        """Tests the main function of the Host Progression Sweep.
+        Person 1 is set to susceptible and becoming exposed. Person 2 is set to
+        exposed and becoming infectious in one time step. Checks the
+        population updates as expected. Check that Person 3 stays as
+        susceptible.
+        """
+        mock_latent.return_value = 1.0
+        # First check that people progress through the
+        # infection stages correctly.
+        self.person2.update_status(pe.property.InfectionStatus.Exposed)
+        self.person2.time_of_status_change = 1.0
+        self.person2.next_infection_status = \
+            pe.property.InfectionStatus.InfectMild
+        self.person1.update_status(pe.property.InfectionStatus.Susceptible)
+        self.person1.time_of_status_change = 1.0
+        self.person1.next_infection_status = \
+            pe.property.InfectionStatus.Exposed
+        test_sweep = pe.sweep.HostProgressionSweep()
+        self.state_transition_matrix = pe.Parameters.instance().state_transition_matrix
+        test_sweep.bind_population(self.test_population1)
 
-         # Tests population bound successfully.
-         self.assertEqual(test_sweep._population.cells[0].persons[1].
-                          infection_status, pe.property.InfectionStatus.Exposed)
-
-         test_sweep(1.0)
-         self.assertEqual(self.person2.infection_status,
-                          pe.property.InfectionStatus.InfectMild)
-         self.assertEqual(self.person1.infection_status,
-                          pe.property.InfectionStatus.Exposed)
-         self.assertEqual(self.person3.infection_status,
-                          pe.property.InfectionStatus.Susceptible)
-         self.assertIsInstance(self.person2.time_of_status_change, float)
-         self.assertIsInstance(self.person1.time_of_status_change, float)
-         self.assertTrue(2.0 <= self.person2.time_of_status_change <= 11.0)
-         self.assertTrue(0.0 <= self.person1.time_of_status_change)
-         mock_latent.assert_called_once()
+        # Tests population bound successfully.
+        self.assertEqual(test_sweep._population.cells[0].persons[1].
+                         infection_status, pe.property.InfectionStatus.Exposed)
+        test_sweep(1.0)
+        self.assertEqual(self.person2.infection_status,
+                         pe.property.InfectionStatus.InfectMild)
+        self.assertEqual(self.person1.infection_status,
+                         pe.property.InfectionStatus.Exposed)
+        self.assertEqual(self.person3.infection_status,
+                         pe.property.InfectionStatus.Susceptible)
+        self.assertIsInstance(self.person2.time_of_status_change, float)
+        self.assertIsInstance(self.person1.time_of_status_change, float)
+        self.assertTrue(2.0 <= self.person2.time_of_status_change <= 11.0)
+        self.assertTrue(0.0 <= self.person1.time_of_status_change)
+        mock_latent.assert_called_once()
 
     def test_call_specific(self):
-         """Tests the specific cases in the call method such as people
-         with None as their time to next infection status are the correct
-         infection status and that people who have been set as recovered
-         are dealt with correctly.
-         """
-         # Check assertion error for infection status when None is set
-         # as time to status change.
+        """Tests the specific cases in the call method such as people
+        with None as their time to next infection status are the correct
+        infection status and that people who have been set as recovered
+        are dealt with correctly.
+        """
+        # Check assertion error for infection status when None is set
+        # as time to status change.
 
-         # First reconfigure population and sweep.
-         self.person1.time_of_status_change = None
-         self.person1.update_status(InfectionStatus.Susceptible)
-         self.person2.time_of_status_change = None
-         self.person2.update_status(InfectionStatus.Exposed)
-         self.person3.time_of_status_change = None
-         self.person3.update_status(InfectionStatus.Susceptible)
-         test_sweep = pe.sweep.HostProgressionSweep()
-         test_sweep.bind_population(self.test_population1)
-         test_sweep.state_transition_matrix = pe.Parameters.instance().state_transition_matrix
+        # First reconfigure population and sweep.
+        self.person1.time_of_status_change = None
+        self.person1.update_status(InfectionStatus.Susceptible)
+        self.person2.time_of_status_change = None
+        self.person2.update_status(InfectionStatus.Exposed)
+        self.person3.time_of_status_change = None
+        self.person3.update_status(InfectionStatus.Susceptible)
+        test_sweep = pe.sweep.HostProgressionSweep()
+        test_sweep.bind_population(self.test_population1)
+        test_sweep.state_transition_matrix = pe.Parameters.instance().state_transition_matrix
 
-         # Run sweep and check assertion error is raised for Person 1.
-         with self.assertRaises(AssertionError):
-             test_sweep(1.0)
+        # Run sweep and check assertion error is raised for Person 1.
+        with self.assertRaises(AssertionError):
+            test_sweep(1.0)
 
-         # Reconfigure population and check that Person 1 has the correct
-         # properties on being set as recovered.
-         self.person1.time_of_status_change = 1.0
-         self.person1.update_status(InfectionStatus.InfectMild)
-         self.person1.next_infection_status = InfectionStatus.Recovered
-         self.person2.update_status(InfectionStatus.Susceptible)
-         test_sweep(1.0)
-         self.assertEqual(self.person1.infection_status,
-                          InfectionStatus.Recovered)
-         self.assertEqual(self.person1.next_infection_status, None)
-         self.assertEqual(self.person1.time_of_status_change, np.inf)
+        # Reconfigure population and check that Person 1 has the correct
+        # properties on being set as recovered.
+        self.person1.time_of_status_change = 1.0
+        self.person1.update_status(InfectionStatus.InfectMild)
+        self.person1.next_infection_status = InfectionStatus.Recovered
+        self.person2.update_status(InfectionStatus.Susceptible)
+        test_sweep(1.0)
+        self.assertEqual(self.person1.infection_status,
+                         InfectionStatus.Recovered)
+        self.assertEqual(self.person1.next_infection_status, None)
+        self.assertEqual(self.person1.time_of_status_change, np.inf)
 
-         # Reconfigure population and check that a person is able to progress
-         # infcetion status mutliple times in the same time step. This will be
-         # checked by setting the latent time as 0 so Person 1 should progress
-         # from suscptible to one if the infected statuses in the same time step.
-         pe.Parameters.instance().latent_period_iCDF = np.zeros(21)
-         self.person1.time_of_status_change = 1.0
-         self.person1.update_status(InfectionStatus.Susceptible)
-         self.person1.next_infection_status = InfectionStatus.Exposed
-         test_sweep(1.0)
-         self.assertIn(self.person1.infection_status,
-                          [InfectionStatus.InfectMild, InfectionStatus.InfectASympt, InfectionStatus.InfectGP])
+        # Reconfigure population and check that a person is able to progress
+        # infcetion status mutliple times in the same time step. This will be
+        # checked by setting the latent time as 0 so Person 1 should progress
+        # from suscptible to one if the infected statuses in the same time
+        # step.
+        pe.Parameters.instance().latent_period_iCDF = np.zeros(21)
+        self.person1.time_of_status_change = 1.0
+        self.person1.update_status(InfectionStatus.Susceptible)
+        self.person1.next_infection_status = InfectionStatus.Exposed
+        test_sweep(1.0)
+        self.assertIn(self.person1.infection_status,
+                      [InfectionStatus.InfectMild,
+                       InfectionStatus.InfectASympt,
+                       InfectionStatus.InfectGP])
 
 
 if __name__ == "__main__":
