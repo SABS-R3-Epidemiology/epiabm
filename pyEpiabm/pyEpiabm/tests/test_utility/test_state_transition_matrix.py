3c2ab4ca1d24cbc3131389c236f1299cdf5f1e2b
Patricia Lamirande <patricia.lamirande@dtc.ox.ac.uk>
2022-03-03 10:25:38 +0000

    Added methods to build and fill state transition matrix

--- pyEpiabm/pyEpiabm/tests/test_utility/test_state_transition_matrix.py
+++ pyEpiabm/pyEpiabm/tests/test_utility/test_state_transition_matrix.py
@@ -1,8 +1,6 @@
 import unittest
 from pyEpiabm.utility import StateTransitionMatrix
 
-numReps = 1
-
 
 class TestStateTransitionMatrix(unittest.TestCase):
     """Test the 'StateTransitionMatrix' class.
@@ -11,6 +9,15 @@ class TestStateTransitionMatrix(unittest.TestCase):
         matrix_object = StateTransitionMatrix()
         matrix = matrix_object.build_state_transition_matrix()
         print(matrix)
+        # Should test that elements are equal to 0?
+        # Test dimensions?
+
+    def test_fill_state_transition_matrix(self):
+        matrix_object = StateTransitionMatrix()
+        matrix = matrix_object.build_state_transition_matrix()
+        matrix_object.fill_state_transition_matrix(matrix)
+        print(matrix)
+        # Test sum of rows = 1
 
 
 if __name__ == '__main__':
--- pyEpiabm/pyEpiabm/utility/state_transition_matrix.py
+++ pyEpiabm/pyEpiabm/utility/state_transition_matrix.py
@@ -11,33 +11,59 @@ class StateTransitionMatrix:
     """Class to generate the state transition matrix
     """
     def build_state_transition_matrix(self):
-        """Builds the state transition matrix that is used in the host progression
-        sweep. The rows are associated to the current infection status, the
+        """Builds the structure of the state transition matrix that is used in
+        the host progression sweep. Labels the rows and the columns with the
+        infection status. Fill the rest of the dataframe with zero
+        probabilities of transition.
+
+        :returns: Symmetric matrix in the form of a dataframe
+        :rtype: Pandas dataframe
+        """
+        nb_states = len(InfectionStatus)
+        zero_trans = np.zeros((nb_states, nb_states))
+        init_matrix = pd.DataFrame(zero_trans,
+                                   columns=['Susceptible', 'Exposed',
+                                            'InfectAsympt', 'InfectMild',
+                                            'InfectGP', 'InfectHosp',
+                                            'InfectICU', 'InfectICURecov',
+                                            'Recovered', 'Dead'],
+                                   index=['Susceptible', 'Exposed',
+                                          'InfectAsympt', 'InfectMild',
+                                          'InfectGP', 'InfectHosp',
+                                          'InfectICU', 'InfectICURecov',
+                                          'Recovered', 'Dead'])
+        return init_matrix
+
+    def fill_state_transition_matrix(self, matrix):
+        """Fill the state transition matrix with the non-zeros probabilities.
+        The rows are associated to the current infection status, the
         columns to the next infection status, and the elements are the
         probabilities to go from one state to another. For example, the element
         ij in the matrix is the probability of someone with current infection
         status associated with the row i to move to the infection status
         associated with the columns j.
 
+        :param Matrix: Initialised state transition matrix, with right column
+        and row names. The first column contains the current infection status
+        and all the transition probabilities are set to zero.
+        :type Matrix: Pandas dataframe
         :returns: Matrix in the form of a dataframe
         :rtype: Pandas dataframe
         """
-        nb_states = len(InfectionStatus)
-        init_trans = {'current inf status': ['Susceptible', 'Exposed',
-                                             'InfectAsympt', 'InfectMild',
-                                             'InfectGP', 'InfectHosp',
-                                             'InfectICU', 'InfectICURecov',
-                                             'Recovered', 'Dead'],
-                      'Susceptible': np.zeros(nb_states),
-                      'Exposed': np.zeros(nb_states),
-                      'InfectAsympt': np.zeros(nb_states),
-                      'InfectMild': np.zeros(nb_states),
-                      'InfectGP': np.zeros(nb_states),
-                      'InfectHosp': np.zeros(nb_states),
-                      'InfectICU': np.zeros(nb_states),
-                      'InfectICURecov': np.zeros(nb_states),
-                      'Recovered': np.zeros(nb_states),
-                      'Dead': np.zeros(nb_states)}
-        matrix = pd.DataFrame(init_trans)
-        # We now need to add the non zeros probabilities
+        matrix.loc['Susceptible', 'Exposed'] = 1
+        matrix.loc['Exposed', 'InfectAsympt'] = 0.33
+        matrix.loc['Exposed', 'InfectMild'] = 0.410061048258529
+        matrix.loc['Exposed', 'InfectGP'] = 0.249938951741471
+        matrix.loc['InfectAsympt', 'Recovered'] = 1
+        matrix.loc['InfectMild', 'Recovered'] = 1
+        matrix.loc['InfectGP', 'Recovered'] = 0.837111575271931
+        matrix.loc['InfectGP', 'InfectHosp'] = 0.162888424728069
+        matrix.loc['InfectHosp', 'Recovered'] = 0.44166691836952
+        matrix.loc['InfectHosp', 'InfectICU'] = 0.3969284544
+        matrix.loc['InfectHosp', 'Dead'] = 0.161404627229571
+        matrix.loc['InfectICU', 'InfectICURecov'] = 0.4765104
+        matrix.loc['InfectICU', 'Dead'] = 0.5234896
+        matrix.loc['InfectICURecov', 'Recovered'] = 1
+        matrix.loc['Recovered', 'Recovered'] = 1
+        matrix.loc['Dead', 'Dead'] = 1
         return matrix
