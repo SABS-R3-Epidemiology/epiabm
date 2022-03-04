f69086d9d137ef00dd8bb7c0a717ce5d0492f995
Patricia Lamirande <patricia.lamirande@dtc.ox.ac.uk>
2022-03-04 13:32:31 +0000

    Added tests for transition_time_matrix

--- pyEpiabm/pyEpiabm/tests/test_utility/test_transition_time_matrix.py
+++ pyEpiabm/pyEpiabm/tests/test_utility/test_transition_time_matrix.py
@@ -1,22 +1,40 @@
 import unittest
 
+import pyEpiabm as pe
 from pyEpiabm.utility import TransitionTimeMatrix
-from pyEpiabm.utility import InverseCdf
 import pandas as pd
 import numpy as np
+from pandas.testing import assert_frame_equal
 
 
 class TestTransitionTimeMatrix(unittest.TestCase):
     """Test the 'StateTransitionMatrix' class.
     """
+    def test_build_initial_matrix(self):
+        """Tests the __init__ method by asserting that the initial matrix that is
+        built is equal to the initial matrix expected.
+        """
+        matrix_object = TransitionTimeMatrix()
+        init_matrix = matrix_object.initial_matrix
+        labels = ['Susceptible', 'Exposed', 'InfectASympt', 'InfectMild',
+                  'InfectGP', 'InfectHosp', 'InfectICU', 'InfectICURecov',
+                  'Recovered', 'Dead']
+        zero_filled_dataframe = pd.DataFrame(np.zeros((10, 10)),
+                                             columns=labels, index=labels)
+        assert_frame_equal(init_matrix, zero_filled_dataframe)
 
     def test_fill_state_transition_matrix(self):
+        """Tests the fill_state_transition method by asserting that the matrix
+        is of the right size and that the non-zero elements are of type
+        InverseCdf."""
         matrix_object = TransitionTimeMatrix()
         matrix = matrix_object.fill_transition_time()
-        print(matrix)
-        icdf = matrix.loc['InfectASympt', 'Recovered']
-        chosen_time = icdf.icdf_choose_noexp()
-        print(chosen_time)
+        self.assertEqual(matrix.size, 100)
+        for row in matrix.to_numpy():
+            for element in row:
+                if element != 0:
+                    self.assertIsInstance(element,
+                                          pe.utility.inverse_cdf.InverseCdf)
 
 
 if __name__ == '__main__':
