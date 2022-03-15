bed75f43553631e9ce0ebb1e7bd32bd0145cdf38
Kit Gallagher <gallagherkit@outlook.com>
2022-03-15 15:13:17 +0000

    Testing parameters singleton (#105)

--- pyEpiabm/pyEpiabm/tests/test_core/test_parameters.py
+++ pyEpiabm/pyEpiabm/tests/test_core/test_parameters.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 
 import pyEpiabm as pe
 from pyEpiabm.tests.parameter_config_tests import TestPyEpiabm
@@ -17,6 +18,54 @@ class TestParameters(TestPyEpiabm):
     def test_getattr_fails(self):
         with self.assertRaises(AttributeError):
             pe.Parameters.instance().bbb
+
+
+class TestNoConfigParameters(unittest.TestCase):
+    """Test class for tests without implicit config call
+    in the test class setUp.
+    """
+    def tearDown(self):
+        if pe.Parameters._instance:
+            pe.Parameters._instance = None
+
+    def test_runtime_error(self):
+        with self.assertRaises(RuntimeError):
+            pe.Parameters.instance()
+
+    @patch('json.loads')
+    @patch('builtins.open')
+    def test_set_file(self, mock_open, mock_load):
+        param_loc = ("pyEpiabm/pyEpiabm/tests/"
+                     + "testing_parameters.json")
+        mock_load.return_value = {}
+        pe.Parameters.set_file(param_loc)
+        mock_load.assert_called_once()
+        mock_open.assert_called_once_with(param_loc, 'r')
+
+    @patch('json.loads')
+    def test_read_numbers(self, mock_load):
+        param_loc = ("pyEpiabm/pyEpiabm/tests/"
+                     + "testing_parameters.json")
+        mock_load.return_value = {
+            'val1': 1,
+            'val2': 2.0}
+
+        pe.Parameters.set_file(param_loc)
+        mock_load.assert_called_once()
+        self.assertEqual(pe.Parameters.instance().val1, 1)
+        self.assertEqual(pe.Parameters.instance().val2, 2.0)
+
+    @patch('json.loads')
+    def test_read_lists(self, mock_load):
+        param_loc = ("pyEpiabm/pyEpiabm/tests/"
+                     + "testing_parameters.json")
+        list_val = [0, 0.33, 0.66, 1]
+        mock_load.return_value = {
+            'list1': list_val}
+
+        pe.Parameters.set_file(param_loc)
+        mock_load.assert_called_once()
+        self.assertListEqual(list(pe.Parameters.instance().list1), list_val)
 
 
 if __name__ == '__main__':
