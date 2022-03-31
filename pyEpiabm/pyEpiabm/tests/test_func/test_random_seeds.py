224715219233165f3a2a185578effc05051c12db
Kit Gallagher <gallagherkit@outlook.com>
2022-03-31 11:07:15 +0100

    Functional testing workflow (#122)

---
+++ pyEpiabm/pyEpiabm/tests/test_func/test_random_seeds.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class TestPopulationRandomSeeds(unittest.TestCase):
+    """Placeholder test
+    """
+
+    def test_construct(self):
+        self.assertEqual(1, (3-1))
+        # add random seed tests here
+        # also add whole simulation flow tests etc to separate file
+        # and some workflows to a integration test file
+        # but ensure 100% unit coverage, so if removing tests changes that,
+        # and you can't fix it, don't do that.
+
+
+if __name__ == '__main__':
+    unittest.main()
