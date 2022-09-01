dc60e94d3913fa74d61cd528af47bc05dcfad3f9
Nicholas Fan <nicholas.lj.fan@gmail.com>
2022-09-01 13:35:27 +0100

    Flake8

--- pyEpiabm/pyEpiabm/output/new_cases_writer.py
+++ pyEpiabm/pyEpiabm/output/new_cases_writer.py
@@ -1,6 +1,7 @@
 from pyEpiabm.output._csv_writer import _CsvWriter
 from pyEpiabm.core import Population
 
+
 class NewCasesWriter(_CsvWriter):
     """ Writer for collecting number of daily new cases
     """
@@ -16,8 +17,8 @@ class NewCasesWriter(_CsvWriter):
         super().__init__(
             folder, 'new_cases.csv',
             ['t', 'cell', 'new_cases'], False)
-    
-    def write(self, t:float, population: Population):
+
+    def write(self, t: float, population: Population):
         """ Write method
         Write daily new cases from population to file
 
@@ -31,7 +32,8 @@ class NewCasesWriter(_CsvWriter):
         for cell in population.cells:
             new_cases = 0
             for person in cell.persons:
-                if person.infection_start_time != None and \
+                if \
+                    person.infection_start_time is not None and \
                     person.infection_start_time > (t-1):
-                    new_cases += 1
+                     new_cases += 1
             super().write([t, cell.id, new_cases])
