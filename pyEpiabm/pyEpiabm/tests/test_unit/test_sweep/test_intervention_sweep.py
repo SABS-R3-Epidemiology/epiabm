7dcfa6a40696286971866f7a938440931d946201
Jiayuan Zhu <jiayuanz0322@gmail.com>
2022-12-13 11:30:53 +1100

    add tests for class intervention_sweep

---
+++ pyEpiabm/pyEpiabm/tests/test_unit/test_sweep/test_intervention_sweep.py
@@ -0,0 +1,40 @@
+import unittest
+
+import pyEpiabm as pe
+from pyEpiabm.sweep import InterventionSweep
+from pyEpiabm.property import InfectionStatus
+from pyEpiabm.tests.test_unit.parameter_config_tests import TestPyEpiabm
+
+
+class TestInterventionSweep(TestPyEpiabm):
+    """Test the 'InterventionSweep' class.
+    """
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        super(TestInterventionSweep, cls).setUpClass()
+        cls.interventionsweep = InterventionSweep()
+
+        # construct a popultion with 8 persons
+        cls._population = pe.Population()
+        cls._population.add_cells(1)
+        cls._population.cells[0].add_microcells(1)
+        cls._population.cells[0].microcells[0].add_people(8)
+        for i in range(8):
+            person = cls._population.cells[0].microcells[0].persons[i]
+            person.update_status(InfectionStatus(i + 1))
+        cls.interventionsweep.bind_population(cls._population)
+
+    def test_bind_population(self):
+        self.assertEqual(len(self.interventionsweep.
+                             intervention_params['case_isolation']), 8)
+        self.assertEqual(len(self.interventionsweep.interventions), 1)
+
+    def test___call__(self):
+        self.interventionsweep(time=100)
+        self.assertIsNone(self.interventionsweep._population.cells[0].
+                          persons[0].isolation_start_time)
+
+
+if __name__ == '__main__':
+    unittest.main()
