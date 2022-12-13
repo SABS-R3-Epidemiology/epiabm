5aaf97ff986c609d5ba81247734b6731e7895df7
Jiayuan Zhu <jiayuanz0322@gmail.com>
2022-12-13 11:29:22 +1100

    add tests for intervention class

---
+++ pyEpiabm/pyEpiabm/tests/test_unit/test_intervention/test_abstract_intervention.py
@@ -0,0 +1,44 @@
+import unittest
+
+import pyEpiabm as pe
+from pyEpiabm.intervention import AbstractIntervention
+
+
+class TestAbstractIntervention(unittest.TestCase):
+    """Test the 'AbstractIntervention' class.
+    """
+
+    def test_construct(self):
+        start_time = 1
+        policy_duration = 10
+        threshold = 20
+        population = pe.Population()
+        intervention_object = AbstractIntervention(start_time, policy_duration,
+                                                   threshold, population)
+        self.assertEqual(intervention_object.start_time, 1)
+        self.assertEqual(intervention_object.policy_duration, 10)
+        self.assertEqual(intervention_object.threshold, 20)
+        self.assertEqual(intervention_object._population, population)
+
+    def test_is_active(self):
+        intervention_object = AbstractIntervention(start_time=1,
+                                                   policy_duration=10,
+                                                   threshold=20,
+                                                   population=pe.Population())
+        self.assertTrue(intervention_object.is_active(time=5, num_cases=50))
+        intervention_object = AbstractIntervention(start_time=1,
+                                                   policy_duration=10,
+                                                   threshold=20,
+                                                   population=pe.Population())
+        self.assertFalse(intervention_object.is_active(time=5, num_cases=5))
+
+    def test___call__(self):
+        intervention_object = AbstractIntervention(start_time=1,
+                                                   policy_duration=10,
+                                                   threshold=20,
+                                                   population=pe.Population())
+        self.assertRaises(NotImplementedError, intervention_object.__call__, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
