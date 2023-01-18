7b8e6b507ec0441ddf1410bbdc46c101d2426624
Nathan Schofield <nathan.schofield@dtc.ox.ac.uk>
2023-01-18 14:07:09 +0000

    Basic automatic timestamping file created

--- python_examples/sim_speeds/basic_speed_plot.py
+++ python_examples/sim_speeds/basic_speed_plot.py
@@ -5,11 +5,13 @@ x = []
 y = []
 for line in open('Sim_Speeds_plot.txt', 'r'):
     lines = [i for i in line.split()]
-    x.append(lines[1])
+    x.append(int(lines[1]))
     y.append(int(lines[4]))
       
 plt.title("Simulation time for basic simulation over 60 days")
 plt.xlabel('Population Size')
+plt.xscale('log')
+plt.yscale('log')
 plt.ylabel('time (s)')
 plt.plot(x, y, marker = 'o', c = 'g')
 
