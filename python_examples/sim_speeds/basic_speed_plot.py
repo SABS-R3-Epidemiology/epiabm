4a990ea024e3933cc67b0b36ffc80fcac3a26c92
Nathan Schofield <nathan.schofield@dtc.ox.ac.uk>
2023-01-25 12:06:28 +0000

    Hopefully mergeable

--- python_examples/sim_speeds/basic_speed_plot.py
+++ python_examples/sim_speeds/basic_speed_plot.py
@@ -7,57 +7,34 @@ import matplotlib.pyplot as plt
 
 def get_seconds(time_str):
     print('Time in mm:ss:', time_str)
-    # split in hh, mm, ss
+    # split in  mm, ss
     mm, ss = time_str.split(':')
     return int(mm) * 60 + int(ss)
 
 
-print('Time in Seconds:', get_seconds('05:15'))
-print('Time in Seconds:', get_seconds('00:32'))
-
-x = []
-y_list = []
-y = []
-
-# for line in open(os.path.join(os.path.dirname(__file__),
-#                  "Sim_Speeds.txt"), 'r'):
-#     print('line split:', line.split())
-#     lines = [i for i in range(1, line.split(), 1)]
-#     x.append(int(lines[1]))
-#     y_list.append(lines[4])
-#     y.append(get_seconds(y_list))
-# #    y.append(int(lines[4]))
-
-# with open(os.path.join(os.path.dirname(__file__),
-#                        "Sim_Speeds.txt"), 'r') as input_data:
-#     all_data = [line.strip() for line in input_data.readlines()]
-#     only_data = all_data[1:]
-#     print('DATA! ', all_data)
-#     print('length! ', len(all_data))
-#     print('popu_size ', only_data)
-#     for i in range(1, len(only_data)):
-#         print('i', i)
-#         print('data 1: ', only_data(i,1))
-#         x.append(int(only_data[i,1]))
-#     # y_list.append(lines[4])
-#         y.append(int(get_seconds(only_data[i,4])))
-# #    y.append(int(lines[4]))
-
 data_path = os.path.join(os.path.dirname(__file__), "Sim_Speeds.txt")
-df = pd.read_csv(data_path, header = 0, sep = "\s\s+")
+
+df = pd.read_csv(data_path, header=0, sep=r"\s\s+")
 df['Seconds'] = df['Time'].apply(get_seconds)
 print(df.head())
 
+x = []
+y = []
+
+x = df['Pop size']
+y = df['Seconds']
 
 
+plt.plot(x, y, marker='o', c='g')
 plt.title("Simulation time for basic simulation over 60 days")
 plt.xlabel('Population Size')
 plt.xscale('log')
 plt.yscale('log')
 plt.ylabel('time (s)')
-plt.plot(x, y, marker='o', c='g')
 
-plt.savefig("sim_speeds_plots/basic_sim_speed.png")
+
+plt.savefig(os.path.join(os.path.dirname(__file__),
+            "sim_speeds_plots/", "basic_sim_speed_from_file.png"))
 
 
 plt.show()
