8570525c2480e1de8a1afd8eeba94af80087e40a
kcgallagher <christopher.gallagher@seh.ox.ac.uk>
2023-01-25 11:53:19 +0000

    A painfree guide to reading in files

--- python_examples/sim_speeds/basic_speed_plot.py
+++ python_examples/sim_speeds/basic_speed_plot.py
@@ -1,7 +1,8 @@
 # Code to plot runtime from .txt file
 
-import matplotlib.pyplot as plt
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def get_seconds(time_str):
@@ -27,20 +28,27 @@ y = []
 #     y.append(get_seconds(y_list))
 # #    y.append(int(lines[4]))
 
-with open(os.path.join(os.path.dirname(__file__),
-                       "Sim_Speeds.txt"), 'r') as input_data:
-    all_data = [line.strip() for line in input_data.readlines()]
-    only_data = all_data[1:]
-    print('DATA! ', all_data)
-    print('length! ', len(all_data))
-    print('popu_size ', only_data)
-    for i in range(1, len(only_data)):
-        print('i', i)
-        print('data 1: ', only_data(i,1))
-        x.append(int(only_data[i,1]))
-    # y_list.append(lines[4])
-        y.append(int(get_seconds(only_data[i,4])))
-#    y.append(int(lines[4]))
+# with open(os.path.join(os.path.dirname(__file__),
+#                        "Sim_Speeds.txt"), 'r') as input_data:
+#     all_data = [line.strip() for line in input_data.readlines()]
+#     only_data = all_data[1:]
+#     print('DATA! ', all_data)
+#     print('length! ', len(all_data))
+#     print('popu_size ', only_data)
+#     for i in range(1, len(only_data)):
+#         print('i', i)
+#         print('data 1: ', only_data(i,1))
+#         x.append(int(only_data[i,1]))
+#     # y_list.append(lines[4])
+#         y.append(int(get_seconds(only_data[i,4])))
+# #    y.append(int(lines[4]))
+
+data_path = os.path.join(os.path.dirname(__file__), "Sim_Speeds.txt")
+df = pd.read_csv(data_path, header = 0, sep = "\s\s+")
+df['Seconds'] = df['Time'].apply(get_seconds)
+print(df.head())
+
+
 
 plt.title("Simulation time for basic simulation over 60 days")
 plt.xlabel('Population Size')
