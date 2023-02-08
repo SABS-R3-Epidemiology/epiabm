41f66f5c45b1948fd95fbc10c14f4dc8d3eedc5d
Lara Herriott <58111273+laraherriott@users.noreply.github.com>
2023-02-08 11:10:21 +0000

    Change in line with reading **params in

--- pyEpiabm/pyEpiabm/intervention/vaccination.py
+++ pyEpiabm/pyEpiabm/intervention/vaccination.py
@@ -13,18 +13,17 @@ class Vaccination(AbstractIntervention):
         self,
         daily_doses,
         population,
-        *args,
         **kwargs
     ):
         self.daily_doses = daily_doses
 
         # start_time, policy_duration, threshold, population
-        super(Vaccination, self).__init__(population=population, *args,
+        super(Vaccination, self).__init__(population=population,
                                           **kwargs)
 
     def __call__(self, time):
         number_vaccinated = 0
-        while number_vaccinated < self.vaccines_per_day and not self._population.vaccine_queue.empty():
+        while number_vaccinated < self.daily_doses and not self._population.vaccine_queue.empty():
             person = self._population.vaccine_queue.get()[2]
             person.is_vaccinated = True
             person.date_vaccinated = time
