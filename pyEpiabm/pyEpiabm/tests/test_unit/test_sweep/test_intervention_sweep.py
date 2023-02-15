508566c608504e26416fa61b79c2e5442b50c3b2
HenrietteCapel <henriette_capel@live.nl>
2023-02-15 11:35:17 +0000

    changed back to only testing closure_start_time and changed setup of population such that runs for household quarantine intervention

--- pyEpiabm/pyEpiabm/tests/test_unit/test_sweep/test_intervention_sweep.py
+++ pyEpiabm/pyEpiabm/tests/test_unit/test_sweep/test_intervention_sweep.py
@@ -2,6 +2,7 @@ import unittest
 
 import pyEpiabm as pe
 from pyEpiabm.sweep import InterventionSweep
+from pyEpiabm.property import InfectionStatus
 from pyEpiabm.tests.test_unit.parameter_config_tests import TestPyEpiabm
 
 
@@ -19,6 +20,9 @@ class TestInterventionSweep(TestPyEpiabm):
         cls.pop_params = {"population_size": 8, "cell_number": 1,
                           "microcell_number": 1, "household_number": 1}
         cls._population = cls.pop_factory.make_pop(cls.pop_params)
+        for i in range(8):
+            person = cls._population.cells[0].microcells[0].persons[i]
+            person.update_status(InfectionStatus(i + 1))
         cls.interventionsweep.bind_population(cls._population)
 
     def test_bind_population(self):
@@ -32,12 +36,8 @@ class TestInterventionSweep(TestPyEpiabm):
 
     def test___call__(self):
         self.interventionsweep(time=100)
-        self.assertIsNone(self.interventionsweep._population.cells[0].
-                          persons[0].isolation_start_time)
         self.assertIsNotNone(self.interventionsweep._population.cells[0].
                              microcells[0].closure_start_time)
-        self.assertIsNone(self.interventionsweep._population.cells[0].
-                          persons[0].quarantine_start_time)
 
 
 if __name__ == '__main__':
