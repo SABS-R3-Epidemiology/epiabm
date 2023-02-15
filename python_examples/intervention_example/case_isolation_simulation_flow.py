f36211db6e5d61c01a3d84c2281703391425245f
HenrietteCapel <henriette_capel@live.nl>
2023-02-15 16:40:42 +0000

    Run intervention examples in a loop

--- python_examples/intervention_example/case_isolation_simulation_flow.py
+++ python_examples/intervention_example/case_isolation_simulation_flow.py
@@ -29,214 +29,90 @@ name_parameter_file = 'case_isolation_parameters.json'
 pe.Parameters.set_file(os.path.join(os.path.dirname(__file__),
                        name_parameter_file))
 
-##########################
-# 0% isolating probability
-name_output_file = 'output_0isolate.csv'
+# Parameter to change
+to_modify_parameter = 'isolation_probability'
+parameter_values = [0.0, 0.5, 1.0]
 
-# Set parameter
-pe.Parameters.instance().intervention_params['case_isolation'][
-    'isolation_probability'] = 0.0
-print('Set isolation_probability to: {}'.format(
-      pe.Parameters.instance().intervention_params['case_isolation'][
-        'isolation_probability']))
+for i in range(len(parameter_values)):
+    name_output_file = 'output_{}_{}'.format(
+        int(parameter_values[i]*100), to_modify_parameter)
 
-# Method to set the seed at the start of the simulation, for reproducibility
-pe.routine.Simulation.set_random_seed(seed=30)
+    pe.Parameters.instance().intervention_params['case_isolation'][
+        to_modify_parameter] = parameter_values[i]
+    print('Set isolation_probability to: {}'.format(
+        pe.Parameters.instance().intervention_params['case_isolation'][
+            to_modify_parameter]))
 
-# Create population
-population = pe.routine.FilePopulationFactory.make_pop(file_loc,
-                                                       random_seed=42)
+    # Method to set the seed at the start of the simulation,
+    # for reproducibility
+    pe.routine.Simulation.set_random_seed(seed=30)
 
-# Configure population with input data
-pe.routine.ToyPopulationFactory.add_places(population, 1)
+    # Create population
+    population = pe.routine.FilePopulationFactory.make_pop(
+        file_loc, random_seed=42)
 
-# file_params give details for where output should be written to.
-file_params = {"output_file": name_output_file,
-               "output_dir": os.path.join(os.path.dirname(__file__),
-                                          "intervention_outputs"),
-               "spatial_output": True}
+    # Configure population with input data
+    pe.routine.ToyPopulationFactory.add_places(population, 1)
 
-# Create a simulation object, configure it with the parameters given, then
-# run the simulation.
-sim = pe.routine.Simulation()
-sim.configure(
-    population,
-    [pe.sweep.InitialInfectedSweep(), pe.sweep.InitialisePlaceSweep()],
-    [
-        pe.sweep.InterventionSweep(),
-        pe.sweep.UpdatePlaceSweep(),
-        pe.sweep.HouseholdSweep(),
-        pe.sweep.PlaceSweep(),
-        pe.sweep.SpatialSweep(),
-        pe.sweep.QueueSweep(),
-        pe.sweep.HostProgressionSweep(),
-    ],
-    sim_params,
-    file_params,
-)
-sim.run_sweeps()
+    # file_params give details for where output should be written to.
+    file_params = {"output_file": name_output_file,
+                   "output_dir": os.path.join(os.path.dirname(__file__),
+                                              "intervention_outputs"),
+                   "spatial_output": True}
 
-# Need to close the writer object at the end of each simulation.
-del sim.writer
-del sim
+    # Create a simulation object, configure it with the parameters given, then
+    # run the simulation.
+    sim = pe.routine.Simulation()
+    sim.configure(
+        population,
+        [pe.sweep.InitialInfectedSweep(), pe.sweep.InitialisePlaceSweep()],
+        [
+            pe.sweep.InterventionSweep(),
+            pe.sweep.UpdatePlaceSweep(),
+            pe.sweep.HouseholdSweep(),
+            pe.sweep.PlaceSweep(),
+            pe.sweep.SpatialSweep(),
+            pe.sweep.QueueSweep(),
+            pe.sweep.HostProgressionSweep(),
+        ],
+        sim_params,
+        file_params,
+    )
+    sim.run_sweeps()
 
-###########################
-# 50% isolating probability
-name_output_file = 'output_50isolate.csv'
-
-# Set parameter
-pe.Parameters.instance().intervention_params['case_isolation'][
-    'isolation_probability'] = 0.5
-print('Set isolation_probability to: {}'.format(
-      pe.Parameters.instance().intervention_params['case_isolation'][
-        'isolation_probability']))
-
-# Method to set the seed at the start of the simulation, for reproducibility
-pe.routine.Simulation.set_random_seed(seed=30)
-
-# Create population
-population = pe.routine.FilePopulationFactory.make_pop(file_loc,
-                                                       random_seed=42)
-
-# Configure population with input data
-pe.routine.ToyPopulationFactory.add_places(population, 1)
-
-# file_params give details for where output should be written to.
-file_params = {"output_file": name_output_file,
-               "output_dir": os.path.join(os.path.dirname(__file__),
-                                          "intervention_outputs"),
-               "spatial_output": True}
-
-# Create a simulation object, configure it with the parameters given, then
-# run the simulation.
-sim = pe.routine.Simulation()
-sim.configure(
-    population,
-    [pe.sweep.InitialInfectedSweep(), pe.sweep.InitialisePlaceSweep()],
-    [
-        pe.sweep.InterventionSweep(),
-        pe.sweep.UpdatePlaceSweep(),
-        pe.sweep.HouseholdSweep(),
-        pe.sweep.PlaceSweep(),
-        pe.sweep.SpatialSweep(),
-        pe.sweep.QueueSweep(),
-        pe.sweep.HostProgressionSweep(),
-    ],
-    sim_params,
-    file_params,
-)
-sim.run_sweeps()
-
-# Need to close the writer object at the end of each simulation.
-del sim.writer
-del sim
-
-############################
-# 100% isolating probability
-name_output_file = 'output_100isolate.csv'
-
-# Set parameter
-pe.Parameters.instance().intervention_params['case_isolation'][
-    'isolation_probability'] = 1.0
-print('Set isolation_probability to: {}'.format(
-      pe.Parameters.instance().intervention_params['case_isolation'][
-        'isolation_probability']))
-
-# Method to set the seed at the start of the simulation, for reproducibility
-pe.routine.Simulation.set_random_seed(seed=30)
-
-# Create population
-population = pe.routine.FilePopulationFactory.make_pop(file_loc,
-                                                       random_seed=42)
-
-# Configure population with input data
-pe.routine.ToyPopulationFactory.add_places(population, 1)
-
-# file_params give details for where output should be written to.
-file_params = {"output_file": name_output_file,
-               "output_dir": os.path.join(os.path.dirname(__file__),
-                                          "intervention_outputs"),
-               "spatial_output": True}
-
-# Create a simulation object, configure it with the parameters given, then
-# run the simulation.
-sim = pe.routine.Simulation()
-sim.configure(
-    population,
-    [pe.sweep.InitialInfectedSweep(), pe.sweep.InitialisePlaceSweep()],
-    [
-        pe.sweep.InterventionSweep(),
-        pe.sweep.UpdatePlaceSweep(),
-        pe.sweep.HouseholdSweep(),
-        pe.sweep.PlaceSweep(),
-        pe.sweep.SpatialSweep(),
-        pe.sweep.QueueSweep(),
-        pe.sweep.HostProgressionSweep(),
-    ],
-    sim_params,
-    file_params,
-)
-sim.run_sweeps()
-
-# Need to close the writer object at the end of each simulation.
-del sim.writer
-del sim
+    # Need to close the writer object at the end of each simulation.
+    del sim.writer
+    del sim
 
 ###############################
 # Creation of a plot of results
 logging.getLogger("matplotlib").setLevel(logging.WARNING)
 
-dict_filenames = {}
-for i in [0, 50, 100]:
-    dict_filenames["filename_" + str(i) + 'isolate'] =\
-        os.path.join(os.path.dirname(__file__),
-                     "intervention_outputs",
-                     "output_{}isolate.csv".format(i))
+for i in range(len(parameter_values)):
+    file_name = os.path.join(os.path.dirname(__file__),
+                             "intervention_outputs",
+                             'output_{}_{}'.format(
+                                int(parameter_values[i]*100),
+                                to_modify_parameter))
+    df = pd.read_csv(file_name)
+    total_df = \
+        df[list(df.filter(regex='InfectionStatus.Infect'))]
+    df["Infected"] = total_df.sum(axis=1)
+    df = df.groupby(["time"]).agg(
+                {"InfectionStatus.Susceptible": 'sum',
+                 "Infected": 'sum',
+                 "InfectionStatus.Recovered": 'sum',
+                 "InfectionStatus.Dead": 'sum'})
+    df = df.reset_index(level=0)
 
-df_0isolate = pd.read_csv(dict_filenames['filename_0isolate'])
-df_50isolate = pd.read_csv(dict_filenames['filename_50isolate'])
-df_100isolate = pd.read_csv(dict_filenames['filename_100isolate'])
-
-total_0isolate = \
-    df_0isolate[list(df_0isolate.filter(regex='InfectionStatus.Infect'))]
-df_0isolate["Infected"] = total_0isolate.sum(axis=1)
-df_0isolate = df_0isolate.groupby(["time"]).agg(
-                                {"InfectionStatus.Susceptible": 'sum',
-                                 "Infected": 'sum',
-                                 "InfectionStatus.Recovered": 'sum',
-                                 "InfectionStatus.Dead": 'sum'})
-df_0isolate = df_0isolate.reset_index(level=0)
-
-total_50isolate = \
-    df_50isolate[list(df_50isolate.filter(regex='InfectionStatus.Infect'))]
-df_50isolate["Infected"] = total_50isolate.sum(axis=1)
-df_50isolate = df_50isolate.groupby(["time"]).agg(
-                                {"InfectionStatus.Susceptible": 'sum',
-                                 "Infected": 'sum',
-                                 "InfectionStatus.Recovered": 'sum',
-                                 "InfectionStatus.Dead": 'sum'})
-df_50isolate = df_50isolate.reset_index(level=0)
-
-total_100isolate = \
-    df_100isolate[list(df_100isolate.filter(regex='InfectionStatus.Infect'))]
-df_100isolate["Infected"] = total_100isolate.sum(axis=1)
-df_100isolate = df_100isolate.groupby(["time"]).agg(
-                                {"InfectionStatus.Susceptible": 'sum',
-                                 "Infected": 'sum',
-                                 "InfectionStatus.Recovered": 'sum',
-                                 "InfectionStatus.Dead": 'sum'})
-df_100isolate = df_100isolate.reset_index(level=0)
-
-plt.plot(df_0isolate['time'], df_0isolate['Infected'], label='0% isolating')
-plt.plot(df_50isolate['time'], df_50isolate['Infected'],
-         label='50% isolating')
-plt.plot(df_100isolate['time'], df_100isolate['Infected'],
-         label='100% isolating')
+    plt.plot(df['time'], df['Infected'], label='{}% isolating'.format(
+        int(parameter_values[i]*100)))
 
 plt.legend()
-plt.title("Infection curves for different isolating probabilities")
+plt.title("Infection curves for different case isolating probabilities")
 plt.ylabel("Infected Population")
 plt.savefig(
     os.path.join(os.path.dirname(__file__),
-                 "intervention_outputs", "case_isolation_Icurve_plot.png")
+                 "intervention_outputs",
+                 "case_isolation_Icurve_plot.png")
 )
--- python_examples/intervention_example/household_quarantine_simulation_flow.py
+++ python_examples/intervention_example/household_quarantine_simulation_flow.py
@@ -1,5 +1,5 @@
 #
-# Example simulation script with data output
+# Example simulation script with household quarantine intervention data output
 # and visualisation
 #
 
@@ -29,214 +29,87 @@ name_parameter_file = 'household_quarantine_parameters.json'
 pe.Parameters.set_file(os.path.join(os.path.dirname(__file__),
                        name_parameter_file))
 
-###############################
-# 0% quarantine house compliant
-name_output_file = 'output_0hcompliant.csv'
+# Parameter to change
+to_modify_parameter = 'quarantine_house_compliant'
+parameter_values = [0.0, 0.5, 1.0]
 
-pe.Parameters.instance().intervention_params['household_quarantine'][
-    'quarantine_house_compliant'] = 0.0
-print('Set quarantine_house_compliant to: {}'.format(
-      pe.Parameters.instance().intervention_params['household_quarantine'][
-        'quarantine_house_compliant']))
+for i in range(len(parameter_values)):
+    name_output_file = 'output_{}_{}'.format(
+        int(parameter_values[i]*100), to_modify_parameter)
 
-# Method to set the seed at the start of the simulation, for reproducibility
-pe.routine.Simulation.set_random_seed(seed=30)
+    pe.Parameters.instance().intervention_params['household_quarantine'][
+        to_modify_parameter] = parameter_values[i]
+    print('Set quarantine_house_compliant to: {}'.format(
+        pe.Parameters.instance().intervention_params['household_quarantine'][
+            to_modify_parameter]))
 
-# Create population
-population = pe.routine.FilePopulationFactory.make_pop(file_loc,
-                                                       random_seed=42)
+    # Method to set the seed at the start of the simulation,
+    # for reproducibility
+    pe.routine.Simulation.set_random_seed(seed=30)
 
-# Configure population with input data
-pe.routine.ToyPopulationFactory.add_places(population, 1)
+    # Create population
+    population = pe.routine.FilePopulationFactory.make_pop(
+        file_loc, random_seed=42)
 
-# file_params give details for where output should be written to.
-file_params = {"output_file": name_output_file,
-               "output_dir": os.path.join(os.path.dirname(__file__),
-                                          "intervention_outputs"),
-               "spatial_output": True}
+    # Configure population with input data
+    pe.routine.ToyPopulationFactory.add_places(population, 1)
 
-# Create a simulation object, configure it with the parameters given, then
-# run the simulation.
-sim = pe.routine.Simulation()
-sim.configure(
-    population,
-    [pe.sweep.InitialInfectedSweep(), pe.sweep.InitialisePlaceSweep()],
-    [
-        pe.sweep.InterventionSweep(),
-        pe.sweep.UpdatePlaceSweep(),
-        pe.sweep.HouseholdSweep(),
-        pe.sweep.PlaceSweep(),
-        pe.sweep.SpatialSweep(),
-        pe.sweep.QueueSweep(),
-        pe.sweep.HostProgressionSweep(),
-    ],
-    sim_params,
-    file_params,
-)
-sim.run_sweeps()
+    # file_params give details for where output should be written to.
+    file_params = {"output_file": name_output_file,
+                   "output_dir": os.path.join(os.path.dirname(__file__),
+                                              "intervention_outputs"),
+                   "spatial_output": True}
 
-# Need to close the writer object at the end of each simulation.
-del sim.writer
-del sim
+    # Create a simulation object, configure it with the parameters given, then
+    # run the simulation.
+    sim = pe.routine.Simulation()
+    sim.configure(
+        population,
+        [pe.sweep.InitialInfectedSweep(), pe.sweep.InitialisePlaceSweep()],
+        [
+            pe.sweep.InterventionSweep(),
+            pe.sweep.UpdatePlaceSweep(),
+            pe.sweep.HouseholdSweep(),
+            pe.sweep.PlaceSweep(),
+            pe.sweep.SpatialSweep(),
+            pe.sweep.QueueSweep(),
+            pe.sweep.HostProgressionSweep(),
+        ],
+        sim_params,
+        file_params,
+    )
+    sim.run_sweeps()
 
-################################
-# 50% quarantine house compliant
-name_output_file = 'output_50hcompliant.csv'
-
-pe.Parameters.instance().intervention_params['household_quarantine'][
-    'quarantine_house_compliant'] = 0.5
-print('Set quarantine_house_compliance to: {}'.format(
-      pe.Parameters.instance().intervention_params['household_quarantine'][
-        'quarantine_house_compliant']))
-
-# Method to set the seed at the start of the simulation, for reproducibility
-pe.routine.Simulation.set_random_seed(seed=30)
-
-# Create population
-population = pe.routine.FilePopulationFactory.make_pop(file_loc,
-                                                       random_seed=42)
-
-# Configure population with input data
-pe.routine.ToyPopulationFactory.add_places(population, 1)
-
-# file_params give details for where output should be written to.
-file_params = {"output_file": name_output_file,
-               "output_dir": os.path.join(os.path.dirname(__file__),
-                                          "intervention_outputs"),
-               "spatial_output": True}
-
-# Create a simulation object, configure it with the parameters given, then
-# run the simulation.
-sim = pe.routine.Simulation()
-sim.configure(
-    population,
-    [pe.sweep.InitialInfectedSweep(), pe.sweep.InitialisePlaceSweep()],
-    [
-        pe.sweep.InterventionSweep(),
-        pe.sweep.UpdatePlaceSweep(),
-        pe.sweep.HouseholdSweep(),
-        pe.sweep.PlaceSweep(),
-        pe.sweep.SpatialSweep(),
-        pe.sweep.QueueSweep(),
-        pe.sweep.HostProgressionSweep(),
-    ],
-    sim_params,
-    file_params,
-)
-sim.run_sweeps()
-
-# Need to close the writer object at the end of each simulation.
-del sim.writer
-del sim
-
-#################################
-# 100% quarantine house compliant
-name_output_file = 'output_100hcompliant.csv'
-
-pe.Parameters.instance().intervention_params['household_quarantine'][
-    'quarantine_house_compliant'] = 1.0
-print('Set quarantine_house_compliance to: {}'.format(
-      pe.Parameters.instance().intervention_params['household_quarantine'][
-        'quarantine_house_compliant']))
-
-
-# Method to set the seed at the start of the simulation, for reproducibility
-pe.routine.Simulation.set_random_seed(seed=30)
-
-# Create population
-population = pe.routine.FilePopulationFactory.make_pop(file_loc,
-                                                       random_seed=42)
-
-# Configure population with input data
-pe.routine.ToyPopulationFactory.add_places(population, 1)
-
-# file_params give details for where output should be written to.
-file_params = {"output_file": name_output_file,
-               "output_dir": os.path.join(os.path.dirname(__file__),
-                                          "intervention_outputs"),
-               "spatial_output": True}
-
-# Create a simulation object, configure it with the parameters given, then
-# run the simulation.
-sim = pe.routine.Simulation()
-sim.configure(
-    population,
-    [pe.sweep.InitialInfectedSweep(), pe.sweep.InitialisePlaceSweep()],
-    [
-        pe.sweep.InterventionSweep(),
-        pe.sweep.UpdatePlaceSweep(),
-        pe.sweep.HouseholdSweep(),
-        pe.sweep.PlaceSweep(),
-        pe.sweep.SpatialSweep(),
-        pe.sweep.QueueSweep(),
-        pe.sweep.HostProgressionSweep(),
-    ],
-    sim_params,
-    file_params,
-)
-sim.run_sweeps()
-
-# Need to close the writer object at the end of each simulation.
-del sim.writer
-del sim
+    # Need to close the writer object at the end of each simulation.
+    del sim.writer
+    del sim
 
 ###############################
 # Creation of a plot of results
 logging.getLogger("matplotlib").setLevel(logging.WARNING)
 
-dict_filenames = {}
-for i in [0, 50, 100]:
-    dict_filenames["filename_" + str(i) + 'hquarantine'] =\
-        os.path.join(os.path.dirname(__file__),
-                     "intervention_outputs",
-                     "output_{}hcompliant.csv".format(i))
+for i in range(len(parameter_values)):
+    file_name = os.path.join(os.path.dirname(__file__),
+                             "intervention_outputs",
+                             'output_{}_{}'.format(
+                                int(parameter_values[i]*100),
+                                to_modify_parameter))
+    df = pd.read_csv(file_name)
+    total_df = \
+        df[list(df.filter(regex='InfectionStatus.Infect'))]
+    df["Infected"] = total_df.sum(axis=1)
+    df = df.groupby(["time"]).agg(
+                {"InfectionStatus.Susceptible": 'sum',
+                 "Infected": 'sum',
+                 "InfectionStatus.Recovered": 'sum',
+                 "InfectionStatus.Dead": 'sum'})
+    df = df.reset_index(level=0)
 
-df_0hquarantine = pd.read_csv(dict_filenames['filename_0hquarantine'])
-df_50hquarantine = pd.read_csv(dict_filenames['filename_50hquarantine'])
-df_100hquarantine = pd.read_csv(dict_filenames['filename_100hquarantine'])
-
-total_0hquarantine = \
-    df_0hquarantine[list(df_0hquarantine.filter(
-        regex='InfectionStatus.Infect'))]
-df_0hquarantine["Infected"] = total_0hquarantine.sum(axis=1)
-df_0hquarantine = df_0hquarantine.groupby(["time"]).agg(
-                                {"InfectionStatus.Susceptible": 'sum',
-                                 "Infected": 'sum',
-                                 "InfectionStatus.Recovered": 'sum',
-                                 "InfectionStatus.Dead": 'sum'})
-df_0hquarantine = df_0hquarantine.reset_index(level=0)
-
-total_50hquarantine = \
-    df_50hquarantine[list(df_50hquarantine.filter(
-        regex='InfectionStatus.Infect'))]
-df_50hquarantine["Infected"] = total_50hquarantine.sum(axis=1)
-df_50hquarantine = df_50hquarantine.groupby(["time"]).agg(
-                                {"InfectionStatus.Susceptible": 'sum',
-                                 "Infected": 'sum',
-                                 "InfectionStatus.Recovered": 'sum',
-                                 "InfectionStatus.Dead": 'sum'})
-df_50hquarantine = df_50hquarantine.reset_index(level=0)
-
-total_100hquarantine = \
-    df_100hquarantine[list(df_100hquarantine.filter(
-        regex='InfectionStatus.Infect'))]
-df_100hquarantine["Infected"] = total_100hquarantine.sum(axis=1)
-df_100hquarantine = df_100hquarantine.groupby(["time"]).agg(
-                                {"InfectionStatus.Susceptible": 'sum',
-                                 "Infected": 'sum',
-                                 "InfectionStatus.Recovered": 'sum',
-                                 "InfectionStatus.Dead": 'sum'})
-df_100hquarantine = df_100hquarantine.reset_index(level=0)
-
-plt.plot(df_0hquarantine['time'], df_0hquarantine['Infected'],
-         label='0% house compliance')
-plt.plot(df_50hquarantine['time'], df_50hquarantine['Infected'],
-         label='50% house compliance')
-plt.plot(df_100hquarantine['time'], df_100hquarantine['Infected'],
-         label='100% house compliance')
+    plt.plot(df['time'], df['Infected'], label='{}% house compliance'.format(
+        int(parameter_values[i]*100)))
 
 plt.legend()
-plt.title("Infection curves for different house quarantine compliant")
+plt.title("Infection curves for different house quarantine compliance")
 plt.ylabel("Infected Population")
 plt.savefig(
     os.path.join(os.path.dirname(__file__),
