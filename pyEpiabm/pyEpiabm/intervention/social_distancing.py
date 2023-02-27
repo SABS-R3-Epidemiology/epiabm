9134d40ac567ddf4452aa7784554cdd0615939bd
Jiayuan Zhu <jiayuanz0322@gmail.com>
2023-02-27 14:18:34 +0000

    remove first level of compliant in social distancing

--- pyEpiabm/pyEpiabm/intervention/social_distancing.py
+++ pyEpiabm/pyEpiabm/intervention/social_distancing.py
@@ -25,7 +25,6 @@ class SocialDistancing(AbstractIntervention):
         distancing_duration,
         distancing_delay,
         case_microcell_threshold,
-        distancing_compliant,
         distancing_enhanced_prob,
         population,
         **kwargs
@@ -33,7 +32,6 @@ class SocialDistancing(AbstractIntervention):
         self.distancing_duration = distancing_duration
         self.distancing_delay = distancing_delay
         self.case_microcell_threshold = case_microcell_threshold
-        self.distancing_compliant = distancing_compliant
         self.distancing_enhanced_prob = distancing_enhanced_prob
         super(SocialDistancing, self).__init__(population=population,
                                                **kwargs)
@@ -54,13 +52,11 @@ class SocialDistancing(AbstractIntervention):
                                                         distancing_delay
                         for person in microcell.persons:
                             if Parameters.instance().use_ages:
-                                r_compliant = random.random()
-                                if r_compliant < self.distancing_compliant:
-                                    r_age = random.random()
-                                    if r_age < self.distancing_enhanced_prob[
-                                                person.age_group]:
-                                        person.distancing_enhanced = True
-                                    else:
-                                        person.distancing_enhanced = False
+                                r_age = random.random()
+                                if r_age < self.distancing_enhanced_prob[
+                                            person.age_group]:
+                                    person.distancing_enhanced = True
+                                else:
+                                    person.distancing_enhanced = False
                             else:
                                 person.distancing_enhanced = False
