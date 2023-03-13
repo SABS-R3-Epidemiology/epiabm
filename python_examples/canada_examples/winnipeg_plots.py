87b7b9930303ace37c63568d9601cbb5a071a5a7
Nathan Schofield <nathan.schofield@dtc.ox.ac.uk>
2023-03-13 11:23:35 +0000

    Plots code updated

--- python_examples/canada_examples/winnipeg_plots.py
+++ python_examples/canada_examples/winnipeg_plots.py
@@ -9,11 +9,11 @@ from age_stratified_plot import Plotter  # noqa
 # Creation of a plot of results with age stratification
 # if file_params["age_stratified"]:
 p = Plotter(os.path.join(os.path.dirname(__file__),
-            "simulation_outputs/output_bermuda.csv"),
+            "simulation_outputs/output_winnipeg.csv"),
             start_date='18-03-2022', sum_weekly=True)
 p.barchart(os.path.join(os.path.dirname(__file__),
-           "simulation_outputs/age_stratify.png"),
+           "simulation_outputs/winnipeg_age_stratify.png"),
            write_Df_toFile=os.path.join(os.path.dirname(__file__),
-           "simulation_outputs/bermuda_weeky_cases.csv"),
+           "simulation_outputs/winnipeg_weeky_cases.csv"),
            param_file=os.path.join(os.path.dirname(__file__),
-           "bermuda_parameters.json"))
+           "canada_parameters.json"))
