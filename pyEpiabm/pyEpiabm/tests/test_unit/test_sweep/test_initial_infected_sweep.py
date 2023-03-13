ec8b04fc3eeb59a3f24f5c7e792095e9ba19bf79
Lara Herriott <58111273+laraherriott@users.noreply.github.com>
2023-03-13 16:50:48 +0000

    Change for consistent parameter name with readme

--- pyEpiabm/pyEpiabm/tests/test_unit/test_sweep/test_initial_infected_sweep.py
+++ pyEpiabm/pyEpiabm/tests/test_unit/test_sweep/test_initial_infected_sweep.py
@@ -41,7 +41,7 @@ class TestInitialInfectedSweep(TestPyEpiabm):
         # Test asking for more infected people than any single cell contains
         # raises an error.
         params = {"initial_infected_number": 4, "simulation_start_time": 0,
-                  "initial_infected_cell": True}
+                  "initial_infect_cell": True}
         self.assertRaises(ValueError, test_sweep, params)
 
     def test_call(self):
@@ -74,7 +74,7 @@ class TestInitialInfectedSweep(TestPyEpiabm):
 
         # Test functions when initial infected cell given.
         params = {"initial_infected_number": 1, "simulation_start_time": 0,
-                  "initial_infected_cell": 1}
+                  "initial_infect_cell": 1}
         self.person1.update_status(pe.property.InfectionStatus.Susceptible)
         self.person2.update_status(pe.property.InfectionStatus.Susceptible)
         test_sweep(params)
@@ -159,7 +159,7 @@ class TestInitialInfectedSweep(TestPyEpiabm):
         # with all infectious people in one cell
         params = {"simulation_start_time": 0,
                   "initial_infected_number": 4,
-                  "initial_infected_cell": True}
+                  "initial_infect_cell": True}
         test_sweep(params)
         status = pe.property.InfectionStatus.InfectMild
         num_infectious = []
