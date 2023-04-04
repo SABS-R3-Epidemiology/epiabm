5ce85c8d23b6edc486f4c5fe4e11db69bcaefacd
Nathan Schofield <nathan.schofield@dtc.ox.ac.uk>
2023-04-04 11:34:32 +0100

    flake8 changes

--- python_examples/luxembourg_example/luxembourg_inputs/luxembourg_initial_infect.py
+++ python_examples/luxembourg_example/luxembourg_inputs/luxembourg_initial_infect.py
@@ -4,7 +4,8 @@ import random
 input = pd.read_csv('luxembourg_input_file.csv')
 listofzeros = [0] * len(input['location_x'])
 cell_number = 1664
-index_list = input.index[(input['cell'] == cell_number) & (input['Susceptible'] != 0)].tolist()
+index_list = input.index[(input['cell'] == cell_number) & (input['Susceptible']
+                         != 0)].tolist()
 index = random.choice(index_list)
 listofzeros[index] = 1
 input['InfectMild'] = listofzeros
