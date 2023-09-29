dd9d18907193738bb1b447e65efbb88520b08951
HenrietteCapel <henriette_capel@live.nl>
2023-09-29 16:49:49 +0100

    remove seperate toy_intervention directory

--- python_examples/toy_interventions/run_plot.py
+++
@@ -1,7 +0,0 @@
-from toy_plotter import Plotter
-
-# Creation of a plot
-p = Plotter('simulation_outputs', [4, 15], 5, 10, 'isolation_probability',
-            [0.0, 1.0])
-p._summarise_outputs()
-p._multiple_curve_plotter('test3')
