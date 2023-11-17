38b302c9b1c2192b8e5a76eedf135aabb7cda88f
Abbie Evans <abbieevans75@gmail.com>
2023-11-17 15:58:48 +0000

    Updated household id

--- pyEpiabm/pyEpiabm/core/household.py
+++ pyEpiabm/pyEpiabm/core/household.py
@@ -30,7 +30,6 @@ class Household:
             Household's base infectiousness
 
         """
-        self.id = ""
         self.persons = []
         self.susceptible_persons = []
         self.location = loc
@@ -39,6 +38,7 @@ class Household:
         self.cell = microcell.cell
         self.microcell = microcell
         self.isolation_location = False
+        self.id = self.microcell.id + "." + str(len(self.microcell.households))
 
         if not (len(loc) == 2 and isinstance(loc[0], Number) and
                 isinstance(loc[1], Number)):
@@ -119,5 +119,5 @@ class Household:
 
         """
         # May want to set upper limit on the number of digits
-        if re.match("\\d+\\.\\d+\\.\\d+", id):
+        if re.match("^\\d+\\.\\d+\\.\\d+$", id):
             self.id = id
