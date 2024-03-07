4e013d0137f4393f0d467a65bdc44c13bd56f0d5
tomcodewizard <tcr2@talktalk.net>
2024-03-07 11:52:45 +0000

    First changes to parameters

--- python_examples/gibraltar_example/gibraltar_flow.py
+++ python_examples/gibraltar_example/gibraltar_flow.py
@@ -23,7 +23,7 @@ logging.basicConfig(filename='sim.log', filemode='w+', level=logging.DEBUG,
 
 # Set config file for Parameters
 pe.Parameters.set_file(os.path.join(os.path.dirname(__file__),
-                                    "gibraltar_parameters.json"))
+                                    "gibraltar_parameters_without_intervention.json"))
 
 # Generate population from input file
 # (Input converted from CovidSim with `microcell_conversion.py`)
@@ -38,7 +38,7 @@ population = pe.routine.FilePopulationFactory.make_pop(file_loc,
 sim_params = {"simulation_start_time": 0, "simulation_end_time": 90,
               "initial_infected_number": 100, "initial_infect_cell": True,
               "simulation_seed": 42,
-              "include_waning": True}
+              "include_waning": False}
 
 file_params = {"output_file": "output_gibraltar.csv",
                "output_dir": os.path.join(os.path.dirname(__file__),
@@ -67,7 +67,6 @@ sim.configure(
      pe.sweep.InitialisePlaceSweep(),
      pe.sweep.InitialDemographicsSweep(dem_file_params)],
     [
-        pe.sweep.InterventionSweep(),
         pe.sweep.UpdatePlaceSweep(),
         pe.sweep.HouseholdSweep(),
         pe.sweep.PlaceSweep(),
@@ -118,4 +117,4 @@ p.barchart(os.path.join(os.path.dirname(__file__),
            write_Df_toFile=os.path.join(os.path.dirname(__file__),
            "simulation_outputs/gibraltar_weeky_cases.csv"),
            param_file=os.path.join(os.path.dirname(__file__),
-           "gibraltar_parameters.json"))
+           "gibraltar_parameters_without_intervention.json"))
